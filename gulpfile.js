b656818d714eb6fd91351bcb0cbef62d996c9399
QiMiao <qimiao@yunify.com>
2021-03-22 16:13:50 +0800

    feat: Add function of buildIcon for guipfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const util = require('util');
 const gulp = require('gulp');
 const webpack = require('webpack');
 
+const generateSprite = require('./scripts/svg-to-sprite');
+
 const promiseExec = util.promisify(require('child_process').exec);
 const { spawn } = require('child_process');
 const webpackConfig = require('./webpack.config');
@@ -15,7 +17,7 @@ function runWebpack(webpackConfig) {
     webpack(webpackConfig, (err, stats) => {
       if (err || stats.hasErrors()) {
         console.error('err:', err);
-        console.log(stats.toString({ 'errors-warnings': true }))
+        console.log(stats.toString({ 'errors-warnings': true }));
 
         return reject();
       }
@@ -28,22 +30,35 @@ function runWebpack(webpackConfig) {
 }
 
 function copyTemplates() {
-  return gulp.src([
-    './clients/templates/layout.html',
-    './clients/templates/404.html',
-    './clients/templates/500.html',
-  ]).pipe(gulp.dest('./dist/templates'));
+  return gulp
+    .src([
+      './clients/templates/layout.html',
+      './clients/templates/404.html',
+      './clients/templates/500.html',
+    ])
+    .pipe(gulp.dest('./dist/templates'));
 }
 
 function copyImages() {
-  return gulp.src('./clients/assets/images/**/*').pipe(gulp.dest('./dist/images'));
+  return gulp
+    .src('./clients/assets/images/**/*')
+    .pipe(gulp.dest('./dist/images'));
 }
+
+function buildIcons() {
+  return generateSprite();
+}
+
+exports.buildIcons = buildIcons;
 
 exports.webpack = (done) => {
   runWebpack(webpackConfig({ mode: 'production' })).then(done);
-}
+};
 
-exports.build = gulp.series(copyImages, copyTemplates,
+exports.build = gulp.series(
+  copyImages,
+  copyTemplates,
+  buildIcons,
   // (done) => {
   //   runWebpack(webpackConfig({ mode: 'production' })).then(done);
   // },
@@ -51,15 +66,28 @@ exports.build = gulp.series(copyImages, copyTemplates,
   //   return promiseExec('rm -rf docker-files/nginx/dist docker-files/portal/dist', done);
   // },
   () => gulp.src('./dist/**/*').pipe(gulp.dest('./docker-files/nginx/dist')),
-  () => gulp.src('./dist/templates/*').pipe(gulp.dest('./docker-files/portal/dist/templates')),
+  () =>
+    gulp
+      .src('./dist/templates/*')
+      .pipe(gulp.dest('./docker-files/portal/dist/templates'))
 );
 
-exports.default = gulp.series(clean, copyTemplates, copyImages, () => {
-  gulp.watch(['./webpack.config.js'], { ignoreInitial: false }, gulp.series((done) => {
-    runWebpack(webpackConfig({ mode: 'development' })).then(done);
-  }));
+exports.default = gulp.series(
+  clean,
+  copyTemplates,
+  copyImages,
+  buildIcons,
+  () => {
+    gulp.watch(
+      ['./webpack.config.js'],
+      { ignoreInitial: false },
+      gulp.series((done) => {
+        runWebpack(webpackConfig({ mode: 'development' })).then(done);
+      })
+    );
 
-  portalServer = spawn('air');
-  portalServer.stderr.pipe(process.stderr);
-  portalServer.stdout.pipe(process.stdout);
-});
+    portalServer = spawn('air');
+    portalServer.stderr.pipe(process.stderr);
+    portalServer.stdout.pipe(process.stdout);
+  }
+);
