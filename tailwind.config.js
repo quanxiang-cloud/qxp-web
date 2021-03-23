4f57f3ad0f2fc39cb0f7d30fed09e09720211f27
Mark <markduan@yunify.com>
2021-03-23 14:06:33 +0800

    chore: Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,34 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: colors,
-    // spacing: {
-    //   2: '2px',
-    //   4: '4px',
-    //   8: '8px',
-    //   12: '12px',
-    //   16: '16px',
-    //   24: '24px',
-    // },
+    spacing: {
+      2: '2px',
+      4: '4px',
+      8: '8px',
+      12: '12px',
+      16: '16px',
+      24: '24px',
+      36: '36px',
+    },
+    fontSize: {
+      'text-24': ['24px', '32px'],
+      'text-20': ['20px', '38px'],
+      'text-16': ['16px', '24px'],
+      'text-14': ['14px', '22px'],
+      'text-16': ['16px', '24px'],
+      'text-14': ['14px', '22px'],
+      'text-14': ['14px', '22px'],
+      'text-14': ['14px', '22px'],
+      'text-12': ['12px', '20px'],
+    },
+    borderRadius: {
+      2: '2px',
+      4: '4px',
+      8: '8px',
+      12: '12px',
+      // todo remove this
+      dot4: '0.4rem',
+    },
     // disable responsive
     screens: {
       // '2xl': { max: '1535px' },
@@ -25,18 +45,6 @@ module.exports = {
       colors: {
         'white-dot-6-5': 'rgba(255, 255, 255, .65)',
       },
-      // todo remove this
-      spacing: {
-        '16px': '16px',
-      },
-      // spacing: {
-      //   2: '2px',
-      //   4: '4px',
-      //   8: '8px',
-      //   12: '12px',
-      //   16: '16px',
-      //   20: '20px',
-      // },
       width: {
         '2dot4': '2.4rem',
       },
@@ -46,9 +54,6 @@ module.exports = {
       lineHeight: {
         11: '1.1rem',
         'dot-9': '0.9rem',
-      },
-      borderRadius: {
-        dot4: '0.4rem',
       },
       minHeight: {
         'dot-8': '0.8rem',
