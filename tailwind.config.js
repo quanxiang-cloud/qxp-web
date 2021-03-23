db7d68c6aaa43f6516b11794112625a5c2117de7
Mark <markduan@yunify.com>
2021-03-23 11:28:08 +0800

    refactor: Update tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
-const colors = require('./clients/theme/colors');
+const colors = require('./tailwind-config/colors');
 
 module.exports = {
   purge: ['./clients/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: colors,
+    // spacing: {
+    //   2: '2px',
+    //   4: '4px',
+    //   8: '8px',
+    //   12: '12px',
+    //   16: '16px',
+    //   24: '24px',
+    // },
     screens: {
       '2xl': { max: '1535px' },
       xl: { max: '1279px' },
@@ -14,11 +22,20 @@ module.exports = {
     },
     extend: {
       colors: {
-        'white-dot-6-5': 'rgba(255, 255, 255, .65)', 
+        'white-dot-6-5': 'rgba(255, 255, 255, .65)',
       },
+      // todo remove this
       spacing: {
-        '16px': '16px'
+        '16px': '16px',
       },
+      // spacing: {
+      //   2: '2px',
+      //   4: '4px',
+      //   8: '8px',
+      //   12: '12px',
+      //   16: '16px',
+      //   20: '20px',
+      // },
       width: {
         '2dot4': '2.4rem',
       },
