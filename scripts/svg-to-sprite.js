8fad93d14f92b3234316a294c3cfc51c5542dafb
QiMiao <qimiao@yunify.com>
2021-04-06 09:44:02 +0800

    refactor: Change code for sprite svg

--- scripts/svg-to-sprite.js
+++ scripts/svg-to-sprite.js
@@ -12,7 +12,7 @@ function getFileContent(filePath) {
 function getFileFullPath() {
   return new Promise((resolve, reject) => {
     glob(
-      path.join(__dirname, '../clients/portal/components/icon/svgs/*.svg'),
+      path.join(__dirname, '../clients/portal/components/icon/svgs/**/*.svg'),
       (err, files) => {
         if (err) {
           reject(err);
@@ -44,7 +44,6 @@ module.exports = function () {
             .replace(/currentColor/g, 'none')
             .replace(/#475569/g, 'currentColor');
           fs.writeFileSync(
-            // todo minify
             path.join(__dirname, '../dist/images/sprite.svg'),
             svgStrs
           );
