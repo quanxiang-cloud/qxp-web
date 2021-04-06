34853741fbf792693c7adc4944f59cdcfc72915b
Mark <markduan@yunify.com>
2021-04-07 01:13:14 +0800

    refactor: Remove server/pkg/net/user.go

--- server/pkg/portal/handlers/portal.go
+++ server/pkg/portal/handlers/portal.go
@@ -3,12 +3,11 @@ package handlers
 import (
 	"net/http"
 	"qxp-web/server/pkg/contexts"
-	"qxp-web/server/pkg/net"
 )
 
 // PortalHandler render portal page
 func PortalHandler(w http.ResponseWriter, r *http.Request) {
-	userInfo, err := net.GetUserInfo(w, r, GetToken(r))
+	userInfo, err := GetUserInfo(w, r, GetToken(r))
 	if err != nil {
 		tokenKey := getTokenKey(r)
 		refreshTokenKey := getRefreshTokenKey(r)
