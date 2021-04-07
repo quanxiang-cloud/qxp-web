8abded6b50423b271b57e608269a625c95e03fe4
tanjie <starrytan@yunify.com>
2021-04-06 22:53:47 -0700

    feat: Add list-menu drawer pop-confirm

---
+++ clients/components/drawer/index.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import cs from 'classnames';
+
+import Icon from '@c/icon';
+
+import './index.scss';
+
+type Props = {
+  onCancel: () => void;
+  title: string;
+  children: React.ReactNode
+}
+
+function Drawer({ onCancel, title, children }: Props) {
+  const [beganClose, setBeganClose] = useState<boolean>(false);
+  const [visible, setVisible] = useState<boolean>(false);
+  const handleCancel = () => {
+    setBeganClose(true);
+    setTimeout(() => {
+      setVisible(true);
+      onCancel();
+    }, 300);
+  };
+
+  return (
+    <div className={
+      cs('drawer-modal-mask', { 'drawer-began-close': beganClose, 'drawer-close': visible })
+    }>
+      <div className='drawer-content'>
+        <div className='drawer-header header-background-image'>
+          <span className='text-h5'>{title}</span>
+          <Icon onClick={handleCancel} clickable changeable name='close' size={24} />
+        </div>
+        <div className='drawer-main-content'>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Drawer;
