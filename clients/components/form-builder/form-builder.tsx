959c8fe8d117d321f0f44a85488a3ca8c99cd68d
Mark <markduan@yunify.com>
2021-04-13 17:34:56 +0800

    refactor: Move antd config context into form-builder

--- clients/components/form-builder/form-builder.tsx
+++ clients/components/form-builder/form-builder.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { ConfigProvider } from 'antd';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import zhCN from 'antd/lib/locale/zh_CN';
 
 import { StoreContext } from './context';
 import Store from './store';
@@ -15,17 +17,19 @@ type Props = {
 
 function FormBuilder({ className, store }: Props) {
   return (
-    <StoreContext.Provider value={store}>
-      <div className={`form-builder ${className}`}>
-        <DndProvider backend={HTML5Backend} context={window}>
-          <SourceElementPanel />
-          <div className="canvas-wrapper">
-            <Canvas />
-          </div>
-        </DndProvider>
-        <FormSettingPanel />
-      </div>
-    </StoreContext.Provider>
+    <ConfigProvider locale={zhCN}>
+      <StoreContext.Provider value={store}>
+        <div className={`form-builder ${className}`}>
+          <DndProvider backend={HTML5Backend} context={window}>
+            <SourceElementPanel />
+            <div className="canvas-wrapper">
+              <Canvas />
+            </div>
+          </DndProvider>
+          <FormSettingPanel />
+        </div>
+      </StoreContext.Provider>
+    </ConfigProvider>
   );
 }
 
--- clients/portal/pages/form-builder-demo/index.tsx
+++ clients/portal/pages/form-builder-demo/index.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import { FormBuilder } from '@c/form-builder';
 import FormStore from '@c/form-builder/store';
-import { ConfigProvider } from 'antd';
-import zhCN from 'antd/lib/locale/zh_CN';
 import './style.scss';
 
 const FormPage = () => {
   const store = new FormStore({ fields: [] });
 
   return (
-    <ConfigProvider locale={zhCN}>
-      <FormBuilder store={store} className="custom-form-builder" />
-    </ConfigProvider>
+    <FormBuilder store={store} className="custom-form-builder" />
   );
 };
 
