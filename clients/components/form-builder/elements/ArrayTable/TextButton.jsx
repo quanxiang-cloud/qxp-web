0fe6ad1bdc714e5a8deb10be1189c01bf7dbf1f3
Mark <markduan@yunify.com>
2021-04-14 23:26:25 +0800

    chore: Remove clients/components/form-builder/elements

--- clients/components/form-builder/elements/ArrayTable/TextButton.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { Button } from 'antd';
-
-const TextButton = props => <Button type="link" {...props} />;
-export default TextButton;
--- clients/components/form-builder/elements/ColorPicker/AttrForm.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { FormItem } from '@formily/antd';
-import ColorPicker from './ColorPicker';
-
-const AttrForm = () => {
-  return <FormItem label="默认值" name="initialValue" component={ColorPicker} />;
-};
-
-export default AttrForm;
