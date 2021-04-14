207791b11ef1d322b24fdb8ab70ea1901f237001
chaojunyang <chaojunyang@yunify.com>
2021-04-14 18:33:20 +0800

    style: fix button component style

--- clients/lib/notify.ts
+++ clients/lib/notify.ts
@@ -116,7 +116,7 @@ class Notify {
     if (options?.duration === -1) {
       return;
     }
-    // setTimeout(() => this.close(element), options?.duration || this.duration);
+    setTimeout(() => this.close(element), options?.duration || this.duration);
   }
 
   public success(message: string, options?: Options) {
