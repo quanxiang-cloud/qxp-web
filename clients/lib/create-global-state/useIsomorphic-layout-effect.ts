d6f94318d4018d79fe1015ed7c18593ba8849c5a
Mark <markduan@yunify.com>
2021-04-14 23:30:56 +0800

    chore: Remove useless files

--- clients/lib/create-global-state/useIsomorphic-layout-effect.ts
+++
@@ -1,6 +0,0 @@
-import { useEffect, useLayoutEffect } from 'react';
-import { isBrowser } from './util';
-
-const useIsomorphicLayoutEffect = isBrowser ? useLayoutEffect : useEffect;
-
-export default useIsomorphicLayoutEffect;
