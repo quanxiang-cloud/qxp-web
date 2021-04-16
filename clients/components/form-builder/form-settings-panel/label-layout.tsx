707ff814a552a84be8e6f5a7d4557776fe377d2c
QiMiao <qimiao@yunify.com>
2021-04-16 15:35:00 +0800

    feat: Add layout select cssStyle

--- clients/components/form-builder/form-settings-panel/label-layout.tsx
+++ clients/components/form-builder/form-settings-panel/label-layout.tsx
@@ -1,34 +1,65 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import classnames from 'classnames';
+import { observer } from 'mobx-react';
 
+import { StoreContext } from '../context';
 import Icon from '@clients/components/icon';
 
-export default function FormLabelConfig(): JSX.Element {
+function FormLabelConfig(): JSX.Element {
+  const store = useContext(StoreContext);
+
+  function handleClick(value: string):void {
+    store.labelAlign = value;
+  }
+
+  function renderLayoutOption() {
+    return (
+      <>
+        <div
+          className={classnames('content-item', { 'item-focus': store.labelAlign==='left' })}
+          onClick={() => {
+            handleClick('left');
+          }}
+        >
+          <div>
+            <span className="form-label"></span>
+            <span className="form-item-body"></span>
+          </div>
+          <div>
+            <span className="form-label"></span>
+            <span className="form-item-body"></span>
+          </div>
+          <div className="text-center">左右</div>
+          <div className="rectangle"></div>
+          <Icon name='done' type="light" className="ok-icon"/>
+        </div>
+        <div
+          className={classnames('content-item', { 'item-focus': store.labelAlign==='top' })}
+          onClick={() => {
+            handleClick('top');
+          }}
+        >
+          <div>
+            <span className="form-label"></span>
+          </div>
+          <div>
+            <span className="form-item-body" style={{ width: '96px' }}></span>
+          </div>
+          <div className="text-center">上下</div>
+          <div className="rectangle"></div>
+          <Icon name='done' type="light" className="ok-icon"/>
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
       <div className="pt-8">
         <div className="md">
           <h4 className="item-label">字段标题位置</h4>
           <div className="layout-content">
-            <div className="content-item">
-              <div>
-                <span className="form-label"></span>
-                <span className="form-item-body"></span>
-              </div>
-              <div>
-                <span className="form-label"></span>
-                <span className="form-item-body"></span>
-              </div>
-              <div className="text-center">左右</div>
-            </div>
-            <div className="content-item">
-              <div>
-                <span className="form-label"></span>
-              </div>
-              <div>
-                <span className="form-item-body" style={{ width: '96px' }}></span>
-              </div>
-              <div className="text-center">上下</div>
-            </div>
+            {renderLayoutOption()}
           </div>
         </div>
         <div className="md">
@@ -47,3 +78,5 @@ export default function FormLabelConfig(): JSX.Element {
     </>
   );
 }
+
+export default observer(FormLabelConfig);
