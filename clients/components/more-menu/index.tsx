18757f60801164f2c44a74c9683654b127fd6db8
draven <draven@dravendeMacBook-Pro.local>
2021-04-16 15:23:12 +0800

    feature   message table menu reset

--- clients/components/more-menu/index.tsx
+++ clients/components/more-menu/index.tsx
@@ -7,26 +7,36 @@ import Popper from '@c/popper';
 
 export type MenuItem<T> = {
   key: T;
-  label: React.ReactNode;
+  label: React.ReactNode | string;
   disabled?: boolean | undefined;
 }
 
 type Props<T> = {
+  optionsClass?: string;
+  optionsWarpClass?: string;
   menuDesc?: string;
   iconName?: string;
   className?: string;
   onVisibilityChange?: (visible: boolean) => void;
   onChange: (key: T) => void;
+  value?: T;
+  targetClass?: string;
   menus: MenuItem<T>[];
   placement?: Placement;
   likeBtn?: boolean;
-  children?: React.ReactElement;
+  children?: React.ReactElement | JSX.Element;
+  suffix?: JSX.Element | string
 }
 
 type MenuItemsProps<T> = {
+  classname?: string;
   onClick: (key: T) => void;
   menuDesc?: string;
   items: MenuItem<T>[];
+  value?: T;
+  targetClass?: string;
+  optionsClass?: string;
+  suffix?: JSX.Element | string
 }
 
 const modifiers = [
@@ -40,10 +50,10 @@ const modifiers = [
 
 // todo combine with select component
 function RenderMenuItems<T extends React.Key>(
-  { menuDesc, items, onClick }: MenuItemsProps<T>
+  { menuDesc, items, onClick, classname, targetClass, value, optionsClass, suffix }: MenuItemsProps<T>
 ): JSX.Element {
   return (
-    <div className="select-options">
+    <div className={`select-options ${classname}`}>
       {menuDesc && (<p className="select-options__desc">{menuDesc}</p>)}
       {
         items.map(({ key, label, disabled }) => {
@@ -54,11 +64,13 @@ function RenderMenuItems<T extends React.Key>(
                 e.stopPropagation();
                 !disabled && onClick(key);
               }}
-              className={classnames('select-options__option', 'select-option', {
+              className={classnames('select-options__option', 'select-option', 'optionsClass', "select-option__content py-6", {
                 'select-option--disabled': disabled,
-              })}
+                [targetClass||'']: value == key
+              }, optionsClass)}
             >
-              <div className="select-option__content py-6">{label}</div>
+              <span>{label}</span>
+              {(value == key)&&suffix}
             </div>
           );
         })
@@ -70,7 +82,8 @@ function RenderMenuItems<T extends React.Key>(
 // todo fix this
 // opened more-menu will not be closed when another more-menu opened
 export default function MoreMenu<T extends React.Key>({
-  iconName, className, menus, children, onVisibilityChange, onChange, menuDesc, placement,
+  iconName, className, menus, children, onVisibilityChange, onChange, menuDesc, placement, optionsWarpClass,
+  value, targetClass, optionsClass, suffix
 }: Props<T>): JSX.Element {
   // todo fix this ref any type
   const reference = React.useRef<any>(null);
@@ -97,8 +110,13 @@ export default function MoreMenu<T extends React.Key>({
         modifiers={modifiers}
       >
         <RenderMenuItems
+          classname={optionsWarpClass}
           items={menus}
+          value={value} 
+          targetClass={targetClass}
           menuDesc={menuDesc}
+          optionsClass={optionsClass}
+          suffix={suffix}
           onClick={(key): void => {
             popperRef.current?.close();
             onChange(key);
