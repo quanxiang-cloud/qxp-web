7f3686df74a26f84c9e51ae61744be97ed23067f
QiMiao <qimiao@yunify.com>
2021-04-16 16:38:24 +0800

    refactor: Refactor css of form layout setting

--- clients/components/form-builder/form-settings-panel/label-layout.tsx
+++ clients/components/form-builder/form-settings-panel/label-layout.tsx
@@ -16,38 +16,38 @@ function FormLabelConfig(): JSX.Element {
     return (
       <>
         <div
-          className={classnames('content-item', { 'item-focus': store.labelAlign==='left' })}
+          className={classnames('content-item', { 'item-checked': store.labelAlign==='left' })}
           onClick={() => {
             handleClick('left');
           }}
         >
           <div>
-            <span className="form-label"></span>
-            <span className="form-item-body"></span>
+            <span className="item-label"></span>
+            <span className="item-body"></span>
           </div>
           <div>
-            <span className="form-label"></span>
-            <span className="form-item-body"></span>
+            <span className="item-label"></span>
+            <span className="item-body"></span>
           </div>
           <div className="text-center">左右</div>
           <div className="rectangle"></div>
-          <Icon name='done' type="light" className="ok-icon"/>
+          <Icon name='done' type="light" className="check-icon"/>
         </div>
         <div
-          className={classnames('content-item', { 'item-focus': store.labelAlign==='top' })}
+          className={classnames('content-item', { 'item-checked': store.labelAlign==='top' })}
           onClick={() => {
             handleClick('top');
           }}
         >
           <div>
-            <span className="form-label"></span>
+            <span className="item-label"></span>
           </div>
           <div>
-            <span className="form-item-body" style={{ width: '96px' }}></span>
+            <span className="item-body" style={{ width: '96px' }}></span>
           </div>
           <div className="text-center">上下</div>
           <div className="rectangle"></div>
-          <Icon name='done' type="light" className="ok-icon"/>
+          <Icon name='done' type="light" className="check-icon"/>
         </div>
       </>
     );
@@ -56,17 +56,17 @@ function FormLabelConfig(): JSX.Element {
   return (
     <>
       <div className="pt-8">
-        <div className="md">
-          <h4 className="item-label">字段标题位置</h4>
+        <div className="pb-24">
+          <div className="item-title">字段标题位置</div>
           <div className="layout-content">
             {renderLayoutOption()}
           </div>
         </div>
-        <div className="md">
-          <h4 className="item-label">
-            <span>表单提交校验规则:</span>
-            <Icon name="info" />
-          </h4>
+        <div className="pb-24">
+          <div className="item-title">
+            表单提交校验规则:
+            <Icon className='ml-2' name="info" size={18} />
+          </div>
           <div className='page-setting-filter'>
             <Icon className='mr-8' name='add' size={20} />
             添加表单校验规则
