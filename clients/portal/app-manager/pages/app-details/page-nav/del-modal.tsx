a4a972c8795ae0e69b8dc7018c5123287d6a7fe7
Mark <markduan@yunify.com>
2021-04-29 02:18:19 +0800

    style: Footer button in delete page catalog modal

--- clients/portal/app-manager/pages/app-details/page-nav/del-modal.tsx
+++ clients/portal/app-manager/pages/app-details/page-nav/del-modal.tsx
@@ -37,10 +37,9 @@ function DelModal({ onCancel, visible, type = 'page', onOk }: Props) {
       className="static-modal"
       footer={
         (<div className="flex items-center">
-          <Button iconName='close' onClick={onCancel}>
+          <Button iconName='close' onClick={onCancel} className="mr-20">
             取消
           </Button>
-          <div className="px-2"></div>
           <Button
             modifier='primary'
             iconName='check'
