56147bfa3f6f49a645b802d2a5bd18a0321c30d3
Reeves <reevesli@yunify.com>
2021-05-06 16:41:29 +0800

    refactor: change the state management library of the message management table in the system management from Recoil to Mobx

--- clients/portal/modules/system-mgmt/hooks.ts
+++ clients/portal/modules/system-mgmt/hooks.ts
@@ -1,19 +1,29 @@
 import { useEffect } from 'react';
-import { useSetRecoilState, useRecoilValue } from 'recoil';
+import msgMgmt from '@portal/stores/msg-mgmt';
 import { useQuery } from 'react-query';
 import { getMsgList } from '@portal/modules/system-mgmt/api';
 
-import { PageParams, Data, KeyWord, RequestInfo, PageInfo } from './delcare';
-
 export const useInitData = () => {
-  const keyword = useRecoilValue(KeyWord);
-  const pageParams = useRecoilValue(PageParams);
+  const {
+    keyword,
+    pageParams,
+    pageInfo,
+    setRequestInfo,
+    setData,
+    setPageInfo,
+  } = msgMgmt;
 
-  const setRequestInfo = useSetRecoilState(RequestInfo);
-  const setData = useSetRecoilState(Data);
-  const setPageInfo = useSetRecoilState(PageInfo);
-
-  const { isLoading, isError, isFetching, data, refetch } = useQuery(['msg-mgmt-msg-list', keyword, pageParams], () => getMsgList({ ...pageParams, key_word: keyword }));
+  const {
+    isLoading,
+    isError,
+    isFetching,
+    data,
+    refetch,
+  } = useQuery([
+    'msg-mgmt-msg-list',
+    keyword,
+    pageParams,
+  ], () => getMsgList({ ...pageParams, key_word: keyword }));
 
   useEffect(() => {
     setRequestInfo({ isLoading, isError, isFetching });
@@ -22,7 +32,7 @@ export const useInitData = () => {
   useEffect(() => {
     if (!isError) {
       setData(data);
-      data && setPageInfo((current) => ({ ...current, total: data.data.total }));
+      data && setPageInfo({ ...pageInfo, total: data.data.total });
     }
   }, [data, isError]);
 
