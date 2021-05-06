6e80bbb019365fbad4e5c495e7c379b2a2c1b0a8
chaojunyang <chaojunyang@yunify.com>
2021-05-06 18:16:32 +0800

    fix: Replace custom queryfunctionctx with queryfunctioncontext

--- clients/portal/modules/msg-center/api.ts
+++ clients/portal/modules/msg-center/api.ts
@@ -1,15 +1,14 @@
 import fetcher from '@lib/fetcher';
-// import {QueryFunctionContext} from 'react-query';
+import { QueryFunctionContext } from 'react-query';
 import { omitEmpty } from '@portal/utils';
 
 // 获取未读消息列表
-export const getMessageList = async ({ queryKey }: QueryFuncCtx) => {
-  const [, params] = queryKey;
+export const getMessageList = async ({ queryKey }: QueryFunctionContext) => {
   // const defaults = {
   //   read_status: MsgReadStatus.unread,
   //   page: 1,
   // };
-  const { data } = await fetcher.post('/api/v1/message/center/getList', omitEmpty(params));
+  const { data } = await fetcher.post('/api/v1/message/center/getList', omitEmpty(queryKey[1]));
   return data;
 };
 
@@ -20,12 +19,14 @@ export const getUnreadMsgCount = async () => {
 };
 
 // 根据id查询消息详情
-export const getMsgById = async ({ queryKey }: QueryFuncCtx) => {
-  const [, params] = queryKey;
+export const getMsgById = async ({ queryKey }: QueryFunctionContext) => {
+  // const [, params] = queryKey;
   const defaults = {
     read: false, // 是否标记为已读
   };
-  const { data } = await fetcher.post('/api/v1/message/center/getById', { ...defaults, ...params });
+  const { data } = await fetcher.post('/api/v1/message/center/getById',
+    Object.assign(defaults, queryKey[1])
+  );
   return data;
 };
 
