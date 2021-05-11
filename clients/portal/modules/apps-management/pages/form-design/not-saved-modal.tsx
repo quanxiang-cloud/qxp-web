51017b15a0be945467ec9e5dbb784d2ba4cca1de
tanjie <starrytan@yunify.com>
2021-05-11 11:59:25 +0800

    fix: Fix not saved tips

--- clients/portal/modules/apps-management/pages/form-design/not-saved-modal.tsx
+++ clients/portal/modules/apps-management/pages/form-design/not-saved-modal.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { observer } from 'mobx-react';
-import { Modal } from '@QCFE/lego-ui';
 
-import Button from '@c/button';
+import Modal from '@c/modal2';
+
 import store from './store';
 
 type Props = {
@@ -20,28 +20,27 @@ function NotSavedModal({ onCancel }: Props) {
       onCancel();
     });
   };
+
   return (
     <Modal
-      visible
       title='提示'
-      onCancel={onCancel}
-      footer={
-        (<div className="flex flex-row justify-between items-center">
-          <Button
-            onClick={handleCancel}
-            className="mr-20"
-          >
-            否，放弃保存
-          </Button>
-          <Button
-            modifier="primary"
-            onClick={handleSave}
-            disabled={store.saveSchemeLoading}
-          >
-            是，保存更改
-          </Button>
-        </div>)
-      }
+      onClose={onCancel}
+      footerBtnSchema={[
+        {
+          key: '1',
+          text: '否，放弃保存',
+          onClick: handleCancel,
+          className: 'mr-20',
+        },
+        {
+          key: '2',
+          text: '是，保存更改',
+          modifier: 'primary',
+          onClick: handleSave,
+          className: 'mr-20',
+          forbidden: store.saveSchemeLoading,
+        },
+      ]}
     >
       当前有尚未保存的更改，离开当前页面前是否需要保存这些更改？
     </Modal>
