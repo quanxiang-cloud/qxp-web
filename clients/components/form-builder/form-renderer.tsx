f520291a9444eff6dcd520eff6c94deb835609aa
Mark <markduan@yunify.com>
2021-05-13 15:26:32 +0800

    feat: Add form renderer

---
+++ clients/components/form-builder/form-renderer.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { SchemaForm, setValidationLanguage, IForm } from '@formily/antd';
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/lib/locale/zh_CN';
+
+import registry from './registry';
+// todo fix this
+import { visibleHiddenLinkageEffect } from './index';
+import { wrapSchemaByMegaLayout } from './utils';
+
+setValidationLanguage('zh');
+
+type Props = {
+  schema: ISchema;
+  defaultValue?: any;
+  className?: string;
+  onSubmit?: (value: any) => void;
+  onFormValueChange?: (value: any) => void;
+  children?: React.ReactElement | ((form: IForm) => React.ReactElement);
+}
+
+function FormRenderer({ schema, defaultValue, className, onSubmit, onFormValueChange, children }: Props) {
+  return (
+    <ConfigProvider locale={zhCN}>
+      <SchemaForm
+        className={className}
+        onSubmit={onSubmit}
+        onChange={onFormValueChange}
+        defaultValue={defaultValue}
+        components={{ ...registry.components }}
+        schema={wrapSchemaByMegaLayout(schema)}
+        effects={() => {
+          visibleHiddenLinkageEffect(
+            // todo refactor formStore any type
+            schema['x-internal']?.visibleHiddenLinkages || []
+          );
+        }}
+      >
+        {children}
+      </SchemaForm>
+    </ConfigProvider>
+  );
+}
+
+export default FormRenderer;
