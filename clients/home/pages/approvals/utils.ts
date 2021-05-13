e72ef6dd42ee624691b7d66131c97df903d4ff8a
sunnyw <sunnywang@yunify.com>
2021-05-13 15:35:17 +0800

    fix(task-detail): Parse form data with null

--- clients/home/pages/approvals/utils.ts
+++ clients/home/pages/approvals/utils.ts
@@ -1,12 +1,12 @@
 export function parseFormValue(formData: TaskFormData): Record<string, any> {
-  return Object.entries(formData).reduce((acc, [key, { value }]) => {
+  return Object.entries(formData || {}).reduce((acc, [key, { value }]) => {
     acc[key] = value;
     return acc;
   }, {} as Record<string, any>);
 }
 
 export function mapFormDataWithKey(formData: TaskFormData | Record<string, any>): Record<string, any> {
-  return Object.entries(formData).map(([key, item]) => {
+  return Object.entries(formData || {}).map(([key, item]) => {
     return { ...item, keyName: key };
   });
 }
