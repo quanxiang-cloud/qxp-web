1d81059ec2a8a8b4bb95012f2802758790feb4a9
Mark <markduan@yunify.com>
2021-05-18 15:56:57 +0800

    refactor: Move visibleHiddenLinkage to a separate file

---
+++ clients/components/form-builder/linkages/visible-hidden.ts
@@ -0,0 +1,33 @@
+import { createFormActions, FormEffectHooks } from '@formily/antd';
+
+const { onFormValuesChange$ } = FormEffectHooks;
+
+function getComparator(linkage: VisibleHiddenLinkage): Comparator {
+  return (values: Record<string, any>): boolean => {
+    const pairs = linkage.rules.map(({ sourceKey, compareOperator, compareValue }) => {
+      return [values[sourceKey], compareOperator, compareValue];
+    });
+
+    return pairs[linkage.ruleJoinOperator].call(pairs, ([value, compareOperator, compareValue]) => {
+      const executor = new Function('value', 'compareValue', `return value ${compareOperator} compareValue`);
+      return executor(value, compareValue);
+    });
+  };
+}
+
+export default function visibleHiddenLinkageEffect(linkages: VisibleHiddenLinkage[]) {
+  const { setFieldState } = createFormActions();
+
+  const comparatorsAndTargetKey: Array<[Comparator, string]> = linkages.map((linkages) => {
+    // *(abc,def,gij)
+    const targetKeys = `*(${linkages.targetKeys.join(',')})`;
+    return [getComparator(linkages), targetKeys];
+  });
+
+  onFormValuesChange$().subscribe(({ values }) => {
+    comparatorsAndTargetKey.forEach(([comparator, targetKey]) => {
+      const isVisible = comparator(values);
+      setFieldState(targetKey, (state) => state.visible = isVisible);
+    });
+  });
+}
