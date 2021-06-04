d932ebe8c772f6b7111c0e5a424b8df0fd6868f8
Reeves <reevesli@yunify.com>
2021-06-04 10:25:14 +0800

    refactor: add right border in department tree selector

--- tailwind.config.js
+++ tailwind.config.js
@@ -50,6 +50,15 @@ module.exports = {
       14: ['14px', '22px'],
       12: ['12px', '20px'],
     },
+    borderWidth: {
+      DEFAULT: '1px',
+      '0': '0',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '6': '6px',
+      '8': '8px',
+    },
     borderRadius: {
       2: '2px',
       4: '4px',
