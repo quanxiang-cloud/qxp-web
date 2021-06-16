9a2a7173c5761c4462cd6ff036c38d53eac26de7
Mark <markduan@yunify.com>
2021-06-16 11:42:29 +0800

    refactor: Remove useless interface definition

--- clients/components/form-builder/type.d.ts
+++ clients/components/form-builder/type.d.ts
@@ -112,11 +112,6 @@ declare namespace FormBuilder {
     ruleJoinOperator: 'every' | 'some';
     rules: Array<FormDataFilterRule>;
   }
-
-  interface UploadedFile {
-    url: string;
-    filename: string;
-  }
 }
 
 // a copy of formily Schema type definition for reference
