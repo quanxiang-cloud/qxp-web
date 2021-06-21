2bad3c40ebb56e36de4d2fd1e551c7392268e702
Mark <markduan@yunify.com>
2021-06-21 11:01:04 +0000

    Merge branch 'form_0.4/api' into 'master'
    bug
    
    See merge request qxp/qxp-web!249
    
    (cherry picked from commit 4a2b688768d579b72b8d6d7c0e58ee41614e012f)
    
    00b838b5 bug

--- server/pkg/portal/handlers/form_schema.go
+++ server/pkg/portal/handlers/form_schema.go
@@ -306,6 +306,9 @@ func doFill(r *http.Request, vm map[string]interface{}, tableID, fieldNme, cate 
 					return http.StatusInternalServerError, err
 				}
 				ph := fmt.Sprintf("%s%s%s", base, cp.AppID, querySubTableM)
+				if cate == "user" {
+					ph = fmt.Sprintf("%s%s%s", base, cp.AppID, querySubTable)
+				}
 				c1, b1, err := sendRequest2Struct(r, "POST", ph, sr)
 				if err != nil || c1 != http.StatusOK {
 					return c1, err
