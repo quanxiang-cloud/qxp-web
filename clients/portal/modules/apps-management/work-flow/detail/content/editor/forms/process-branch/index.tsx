85b312e2c06869f902caeafdb62860aff984d79c
lishengma <lishengma@yunify.com>
2021-06-23 23:55:29 +0800

    feat: Process branch component

---
+++ clients/portal/modules/apps-management/work-flow/detail/content/editor/forms/process-branch/index.tsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import {
+  createFormActions,
+  SchemaForm, ISchema,
+} from '@formily/antd';
+
+import { Select, Input } from '@formily/antd-components';
+import type { ProcessBranchData, NodeType } from '@flowEditor/type';
+import Tab from '@c/tab';
+import SaveButtonGroup from '@flowEditor/components/_common/action-save-button-group';
+
+import FilterRule from './filter-rule';
+
+export interface Props {
+  nodeType: NodeType;
+  defaultValue: ProcessBranchData;
+  onSubmit: (data: ProcessBranchData) => void;
+  onCancel: () => void;
+}
+
+const formSchema: ISchema = {
+  type: 'object',
+  properties: {
+    ignore: {
+      type: 'boolean',
+      ['x-component']: 'Select',
+      'x-rules': {
+        required: true,
+        message: '请选择筛选条件',
+      },
+      default: false,
+      enum: [{
+        label: '自定义条件',
+        value: false,
+      }, {
+        label: 'else 条件',
+        value: true,
+      }],
+      'x-linkages': [
+        {
+          type: 'value:visible',
+          target: 'rule',
+          condition: '{{ $value === false }}',
+        },
+      ],
+    },
+    rule: {
+      type: 'string',
+      ['x-component']: 'FilterRule',
+      default: '[]',
+    },
+  },
+};
+
+const actions = createFormActions();
+
+const components = {
+  Select,
+  FilterRule,
+  Input,
+};
+
+export default function ProcessBranch(props: Props): JSX.Element {
+  const { defaultValue, onSubmit, onCancel } = props;
+
+  function onSave(): void {
+    actions.getFormState((formState) => {
+      onSubmit(formState.values);
+    });
+  }
+
+  function onClose(): void {
+    onCancel();
+  }
+
+  return (
+    <>
+      <Tab
+        className="mt-10"
+        items={[{
+          id: 'basicConfig',
+          name: '筛选条件',
+          content: (
+            <div className="mt-24">
+              <SchemaForm
+                actions={actions}
+                initialValues={defaultValue}
+                schema={formSchema}
+                components={components}
+              />
+            </div>
+          ),
+        }]}
+      />
+      <SaveButtonGroup onSave={onSave} onCancel={onClose} />
+    </>
+  );
+}
--- clients/portal/modules/apps-management/work-flow/detail/content/editor/nodes/process-branch-source.tsx
+++ clients/portal/modules/apps-management/work-flow/detail/content/editor/nodes/process-branch-source.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Handle, Position } from 'react-flow-renderer';
 
-import FillInNodeComponent from '../components/fill-in';
+import ProcessBranchSourceNodeComponent from '../components/process-branch-source';
 
-export default function FillInNode(props: any): JSX.Element {
+export default function ProcessBranchSourceNode(props: any): JSX.Element {
   return (
     <>
       <Handle
         type="target"
         position={Position.Top}
       />
-      <FillInNodeComponent {...props} />
+      <ProcessBranchSourceNodeComponent {...props} />
       <Handle
         type="source"
         position={Position.Bottom}
