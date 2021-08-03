b7a39ef28d1184704f84aab111319289493a72cb
Mark <markduan@yunify.com>
2021-08-03 02:17:55 +0000

    Merge branch 'hotfix/association-data-config-appID' into 'release/0.5.1'
    fix: Fix association-data config appID
    
    See merge request qxp/qxp-web!534
    
    (cherry picked from commit 67e7ffa3708b338fe9be30611c07a72248200959)
    
    27fb13ff fix: Fix association-data config appID

--- clients/components/form-builder/registry/associated-data/config.tsx
+++ clients/components/form-builder/registry/associated-data/config.tsx
@@ -94,7 +94,7 @@ function AssociatedDataConfig({ initialValue, onChange }: Props): JSX.Element {
       initialValues={initialValue}
       actions={actions}
       effects={formModelEffect}
-      onChange={(values) => onChange({ ...initialValue, ...values })}
+      onChange={(values) => onChange({ ...initialValue, ...values, appID })}
       components={COMPONENTS}
       schema={configSchema}
     />
