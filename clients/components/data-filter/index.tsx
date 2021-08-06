4eb1b906d7705cf4c565978cdf2f7cad0c5c0460
谭杰 <starrytan@yunify.com>
2021-08-06 18:11:38 +0800

    fix(data-filter): Fix data-filter valueForm form bug

--- clients/components/data-filter/index.tsx
+++ clients/components/data-filter/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, forwardRef, useImperativeHandle } from 'react';
 import { useForm, Controller } from 'react-hook-form';
-import moment from 'moment';
 import { uniqueId } from 'lodash';
 
 import Select from '@c/select';
@@ -8,7 +7,15 @@ import FieldSwitch from '@c/field-switch';
 import Icon from '@c/icon';
 import formFieldWrap from '@c/form-field-wrap';
 
-import { CONDITION, getOperators, FILTER_FIELD, getCondition, VALUE_FROM } from './utils';
+import {
+  CONDITION,
+  getOperators,
+  FILTER_FIELD,
+  getCondition,
+  VALUE_FROM,
+  setValueFormCondition,
+  getValue,
+} from './utils';
 import './index.scss';
 
 type Props = {
@@ -33,22 +40,6 @@ export type RefProps = {
   getDataPer: () => Promise<FilterConfig | string>;
   empty: () => void;
   getDataValues: () => FilterConfig
-}
-
-function getValue(field: Fields, initValue: Array<string | number | Date | LabelValue> | undefined) {
-  if (!initValue || initValue.length === 0) {
-    return '';
-  }
-
-  if (field.type === 'datetime') {
-    return Array.isArray(initValue) ? initValue.map((value) => moment(value as string)) : moment(initValue);
-  }
-
-  if (field.enum && field.enum.length) {
-    return initValue;
-  }
-
-  return initValue[0];
 }
 
 const FormFieldSwitch = formFieldWrap({ FieldFC: FieldSwitch });
@@ -87,7 +78,7 @@ function DataFilter({
           id: uniqueId(),
           op: condition.op as string,
           valueFrom: condition.valueFrom,
-          value: getValue(filter, condition.value),
+          value: getValue(filter, condition.value, condition.valueFrom),
           associationFieldsOptions: condition.valueFrom === 'form' ? getAssociationOptions(filter) : [],
           key: condition.key as string,
           filter,
@@ -135,12 +126,22 @@ function DataFilter({
 
   const handleValueFromChange = (rowID: string, valueFrom: string) => {
     setConditions(conditions.map((condition) => {
-      if (condition.id === rowID) {
-        return {
-          ...condition, valueFrom, associationFieldsOptions: getAssociationOptions(condition.filter),
-        } as FieldCondition;
+      if (condition.id !== rowID) {
+        return condition;
       }
-      return condition;
+
+      if (condition.filter?.['x-component'] === 'DatePicker') {
+        condition.filter = {
+          ...condition.filter,
+          type: valueFrom === 'form' ? 'number' : 'datetime',
+        };
+      }
+
+      return {
+        ...condition,
+        valueFrom,
+        associationFieldsOptions: getAssociationOptions(condition.filter),
+      } as FieldCondition;
     }));
     setValue('condition-' + rowID, '');
   };
@@ -171,15 +172,15 @@ function DataFilter({
           return;
         }
 
-        _conditions.push({
-          ...getCondition(
-            condition.filter,
-            formData[`condition-${condition.id}`],
-            condition.filter.id,
-            formData[`operators-${condition.id}`],
-          ),
-          valueFrom: formData[`valueFrom-${condition.id}`] || 'fixedValue',
-        });
+        _conditions.push(
+          setValueFormCondition({
+            valueFrom: formData[`valueFrom-${condition.id}`] || 'fixedValue',
+            key: condition.filter.id,
+            op: formData[`operators-${condition.id}`],
+            value: formData[`condition-${condition.id}`],
+            schema: condition.filter,
+          }),
+        );
       }
     });
 
