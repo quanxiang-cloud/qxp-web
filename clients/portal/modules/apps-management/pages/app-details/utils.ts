41dd06ca15ff414e3bff1c04fc2c99e6f42197a6
tanjie <starrytan@yunify.com>
2021-08-13 14:06:56 +0800

    fix: Fix data-model init-schema

--- clients/portal/modules/apps-management/pages/app-details/utils.ts
+++ clients/portal/modules/apps-management/pages/app-details/utils.ts
@@ -1,5 +1,82 @@
 import { UnionColumns } from 'react-table';
 
+export const SYSTEM_FIELDS: Record<string, ModelFieldSchema> = {
+  _id: {
+    type: 'string',
+    title: 'id',
+    readOnly: false,
+    display: false,
+    'x-component': 'Input',
+    not_null: false,
+    'x-internal': { isSystem: true },
+    'x-index': 0,
+  },
+  created_at: {
+    type: 'datetime',
+    title: '创建时间',
+    readOnly: false,
+    display: false,
+    'x-component': 'DatePicker',
+    not_null: false,
+    'x-component-props': { isNow: false, showTime: false },
+    'x-internal': { isSystem: true },
+    'x-index': 0,
+  },
+  updated_at: {
+    type: 'datetime',
+    title: '修改时间',
+    readOnly: false,
+    display: false,
+    'x-component': 'DatePicker',
+    not_null: false,
+    'x-component-props': { isNow: false, showTime: false },
+    'x-internal': { isSystem: true },
+    'x-index': 0,
+  },
+  creator_name: {
+    type: 'string',
+    title: '创建者',
+    readOnly: false,
+    display: false,
+    not_null: false,
+    'x-component': 'Input',
+    'x-internal': { isSystem: true },
+    'x-index': 0,
+  },
+  creator_id: {
+    type: 'string',
+    title: '创建者 ID',
+    readOnly: false,
+    display: false,
+    'x-component': 'Input',
+    not_null: false,
+    'x-internal': { isSystem: true },
+    'x-index': 0,
+  },
+  modifier_name: {
+    type: 'string',
+    title: '修改者',
+    readOnly: false,
+    display: false,
+    'x-component': 'Input',
+    not_null: false,
+    'x-internal': { isSystem: true },
+    'x-index': 0,
+  },
+  modifier_id: {
+    type: 'string',
+    title: '修改者 ID',
+    readOnly: false,
+    display: false,
+    'x-component': 'Input',
+    not_null: false,
+    'x-internal': { isSystem: true },
+    'x-index': 0,
+  },
+};
+
+
+
 export const FIELD_COLUMNS: UnionColumns<ModelField>[] = [
   {
     Header: '字段编码',
@@ -26,7 +103,7 @@ export const FIELD_COLUMNS: UnionColumns<ModelField>[] = [
 export const INIT_MODEL_SCHEMA = {
   tableID: '',
   schema: {
-    properties: {},
+    properties: SYSTEM_FIELDS,
     title: '',
     type: 'object',
     description: '',
