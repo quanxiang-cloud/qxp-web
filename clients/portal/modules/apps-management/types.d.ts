f5197aad8cd455674490039bce8fc61d934b2490
iriskang <iriskang@yunify.com>
2021-08-17 17:16:14 +0800

    fix: Fix custompage list api

--- clients/portal/modules/apps-management/types.d.ts
+++ clients/portal/modules/apps-management/types.d.ts
@@ -86,6 +86,13 @@ type PageInfo = {
   sort?: number;
 }
 
+type fetchPageListRes = {
+  count: number;
+  menu: PageInfo[];
+}
+
+type fetchPerGroupFormRes = { formArr: { id: string, authority: number }[] }
+
 type PerPageInfo = PageInfo & { authority: number };
 
 type AppParams = {
