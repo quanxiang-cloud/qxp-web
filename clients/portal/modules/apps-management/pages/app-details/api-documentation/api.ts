7c4faf309966fe3d07fb0ef5d48212b3d0773a3e
iris <iriskang@yunify.com>
2021-08-18 10:52:55 +0800

    fix：Fix api-doc api

--- clients/portal/modules/apps-management/pages/app-details/api-documentation/api.ts
+++ clients/portal/modules/apps-management/pages/app-details/api-documentation/api.ts
@@ -2,11 +2,11 @@ import httpClient from '@lib/http-client';
 
 export const getTableXName = (
   appID: string,
-  data: getTableSchemaParam,
+  data: TableSchemaParam,
 ): Promise<{name: string}> => {
   return httpClient(`/api/v1/structor/${appID}/m/table/getXName`, data);
 };
 
-export const getApiDoc = (data: getApiDocParam): Promise<QueryDocRes> => {
-  return httpClient('/api/v1/polyapi/raw/queryDoc', data);
+export const getApiDoc = (apiPath: string, data: ApiDocParam): Promise<QueryDocRes> => {
+  return httpClient(`/api/v1/polyapi/raw/doc${apiPath}`, data);
 };
