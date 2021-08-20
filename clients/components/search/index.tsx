5a79fd0532355ad27d476cd4d50c6316ecec4f9e
Mark <markduan@yunify.com>
2021-08-20 07:48:23 +0000

    Merge branch 'fix/input-style' into 'develop'
    style: Temporarily modify the input style
    
    See merge request qxp/qxp-web!669
    
    (cherry picked from commit 28108250ea0f38ae3a41446273cc15c3cbd970e7)
    
    3cc7715d style: Temporarily modify the input style
    3a4f8deb style: Optimize style display

--- clients/components/search/index.tsx
+++ clients/components/search/index.tsx
@@ -1,6 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, CSSProperties } from 'react';
 import cs from 'classnames';
+
 import Icon from '../icon';
+
+const InputCss: CSSProperties = {
+  background: 'none',
+  width: '122px',
+  height: '20px',
+  boxShadow: 'none',
+  border: 'none',
+};
 
 interface Props {
   value?: string;
@@ -12,33 +21,51 @@ interface Props {
 }
 
 export default function Search(
-  { value: _value, placeholder, onChange, onKeyDown, onBlur, className }: Props) {
+  { value: _value, placeholder, onChange, onKeyDown, onBlur, className }: Props): JSX.Element {
   const [value, setValue] = useState(_value || '');
+  const [focused, setFocused] = useState(false);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>)=>{
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setValue(e.target.value);
     onChange && onChange(e.target.value);
   };
 
-  function handleClick() {
+  function handleClick(): void {
     setValue('');
     onChange && onChange('');
     onBlur && onBlur('');
   }
 
+  function handleInputFocus(): void {
+    setFocused(true);
+  }
+
+  function handleInputBlur(): void {
+    setFocused(false);
+    onBlur && onBlur();
+  }
+
   return (
-    <div className={cs('px-16 py-6 corner-2-8-8-8 bg-white flex items-center', className)}>
+    <div
+      className={cs('px-16 py-5 corner-2-8-8-8 bg-white flex items-center border border-gray-300 ',
+        className,
+        {
+          'input-focus-border': focused,
+        })
+      }
+    >
       <Icon name="search" size={20} className='mr-8' />
       <input
-        style={{ background: 'none', width: 122, boxShadow: 'none' }}
-        className="outline-none flex-grow"
+        style={InputCss}
+        className="flex-grow"
         type="text"
         placeholder={placeholder}
         name="search"
         onChange={handleChange}
         value={value}
         onKeyDown={(e: React.KeyboardEvent) => onKeyDown && onKeyDown(e)}
-        onBlur={() => onBlur && onBlur()}
+        onBlur={handleInputBlur}
+        onFocus={handleInputFocus}
       />
       {value !== '' && (
         <Icon
