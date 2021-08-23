f5dee42b8b452a3449f26130e47c5a712f272d3f
郭峻源 <rexguo@yunify.com>
2021-08-23 18:31:48 +0800

    fix: modify format type

--- clients/components/form-builder/registry/serial-number/effects.ts
+++ clients/components/form-builder/registry/serial-number/effects.ts
@@ -1,6 +1,7 @@
 import { createFormActions, FormEffectHooks } from '@formily/antd';
 import moment from 'moment';
 import { PrefixType } from './convertor';
+import { Format } from './prefix';
 
 type PreviewProps = {
   prefix: PrefixType,
@@ -15,8 +16,25 @@ const addZeroFromValue = (position: number, value:number): string => {
   return (Array(position).join('0') + value?.toString()).slice(-position);
 };
 
+const getMoment = (value: Format | ''): string => {
+  switch (value) {
+  case '':
+    return '';
+  case 'yyyy':
+    return moment().format('YYYY');
+  case 'yyyyMM':
+    return moment().format('YYYYMM');
+  case 'yyyyMMdd':
+    return moment().format('YYYYMMDD');
+  case 'yyyyMMddHHmm':
+    return moment().format('YYYYMMDDhhmm');
+  case 'yyyyMMddHHmmss':
+    return moment().format('YYYYMMDDhhmmss');
+  }
+};
+
 const getPreview = ({ prefix, initialPosition, initialValue, suffix }: PreviewProps): string => {
-  return prefix.frontward + (prefix.backward === '' ? '' : moment().format(prefix.backward)) +
+  return prefix.frontward + getMoment(prefix.backward) +
     addZeroFromValue(initialPosition, initialValue) + suffix;
 };
 
