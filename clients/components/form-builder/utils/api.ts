47be31c7dc3f262bba6f640571f9630f3b4db815
chaojunyang <chaojunyang@yunify.com>
2021-09-15 14:19:36 +0800

    fix: Repair form component filter form

--- clients/components/form-builder/utils/api.ts
+++ clients/components/form-builder/utils/api.ts
@@ -1,42 +1,21 @@
 import logger from '@lib/logger';
 import httpClient, { getTableSchema } from '@lib/http-client';
-import { MenuType } from '@portal/modules/apps-management/pages/app-details/type';
 
-type AppPage = {
+type PageItem = {
   id: string;
   name: string;
-  menuType?: number;
-  child?: AppPage[];
 }
 
-function convertPagesToOptions(
-  appPages: AppPage[],
-  options: Array<{ label: string; value: string }>,
-): Array<{ label: string; value: string }> {
-  appPages.forEach(({ id, name, child, menuType }) => {
-    if (Array.isArray(child)) {
-      convertPagesToOptions(child, options);
-      return;
-    }
-
-    if (menuType === MenuType.schemaForm) {
-      options.push({ label: name, value: id });
-    }
-  });
-
-  return options;
-}
-
-export function getLinkageTables(appID: string): Promise<Array<{ label: string; value: string }>> {
-  return httpClient<{ menu: AppPage[] }>(
-    `/api/v1/structor/${appID}/m/menu/list`,
+export function getLinkageTables(appID: string): Promise<Array<LabelValue>> {
+  return httpClient<{ pages: PageItem[] }>(
+    `/api/v1/structor/${appID}/m/menu/listPage`,
     { appID },
   ).then((res) => {
-    if (!res.menu || !res.menu.length) {
+    if (!res.pages || !res.pages.length) {
       return [];
     }
 
-    return convertPagesToOptions(res.menu, []);
+    return res.pages.map(({ id, name }) => ({ label: name, value: id }));
   }).catch((err) => {
     logger.error(err);
     return [];
