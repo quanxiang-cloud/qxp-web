7aef21871d547e3e7f66ae211a9ce3bc0030b486
郭峻源 <rexguo@yunify.com>
2021-09-26 16:08:30 +0800

    style: Change profile-picture

---
+++ clients/components/profile-picture/index.tsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useRef, useState } from 'react';
+import cs from 'classnames';
+
+import ResetPasswordModal from '@portal/global-header/reset-password-modal';
+import Avatar from '@c/avatar';
+import Icon from '@c/icon';
+import Popper from '@c/popper';
+
+import './index.scss';
+
+interface Props {
+  className?: string;
+}
+
+export default function ProfilePicture({ className }: Props): JSX.Element {
+  const [openResetPasswordModal, setOpenResetPasswordModal] = useState<boolean>(false);
+  const [side, setSide] = useState('');
+  const reference = useRef<HTMLDivElement>(null);
+  const popperRef = React.useRef<Popper>(null);
+
+  useEffect(() => {
+    setSide(window.SIDE);
+  }, []);
+
+  const handleEditPasswordClick = (): void => {
+    setOpenResetPasswordModal(true);
+    popperRef.current?.close();
+  };
+
+  const handleLogOutClick = (): void => {
+    window.location.href = '/logout';
+  };
+
+  return (
+    <div>
+      <ResetPasswordModal
+        visible={openResetPasswordModal}
+        onCancel={() => setOpenResetPasswordModal(false)}
+      />
+      <div
+        className="cursor-pointer flex items-center h-36
+            hover:blue-100 transition group-hover:text-blue-600"
+        ref={reference}
+      >
+        <Avatar
+          username={window.USER.userName}
+        />
+        <Icon name="arrow_drop_down" size={20} />
+      </div>
+      <Popper
+        reference={reference}
+        ref={popperRef}
+      >
+        <div className={cs('avatar', className)}>
+          <div className='avatar-bg flex py-20'>
+            <div className='pl-20'>
+              <Avatar
+                username={window.USER.userName}
+                size={48}
+              />
+            </div>
+            <div className='flex flex-col ml-8'>
+              <span className='text-20 font-medium text-gray-900'>
+                {window.USER.userName}
+              </span>
+              <span className='text-12 text-two'>
+                {side === 'portal' ?
+                  `角色: ${window.USER_ADMIN_ROLES[0].name}` : (window.USER.email || window.USER.phone)}
+              </span>
+            </div>
+          </div>
+          <div className='text-two flex flex-col mx-16 cursor-pointer'>
+            <div
+              className='avatar-border flex py-15 items-center'
+              onClick={handleEditPasswordClick}
+            >
+              <Icon name='password' />
+              <span className='ml-4'>修改密码</span>
+            </div>
+            <div className='flex py-15 items-center' onClick={handleLogOutClick}>
+              <Icon name='logout' />
+              <span className='ml-4'>退出登录</span>
+            </div>
+          </div>
+        </div>
+      </Popper>
+    </div>
+  );
+}
--- clients/home/components/global-header/header-menu.tsx
+++ clients/home/components/global-header/header-menu.tsx
@@ -1,22 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import MoreMenu from '@c/more-menu';
 import NavMsgBar from '@portal/modules/msg-center/nav-msg-bar';
-// import Button from '@c/button';
-import Icon from '@c/icon';
+import ProfilePicture from '@c/profile-picture';
 
-import ResetPasswordModal from './reset-password-modal';
-import Avatar from '@c/avatar';
-
-export default function HeaderMenu() {
-  const [openResetPasswordModal, setOpenResetPasswordModal] = useState<boolean>(false);
-
+export default function HeaderMenu(): JSX.Element {
   return (
     <div className="flex-2 flex justify-end items-center">
-      <ResetPasswordModal
-        visible={openResetPasswordModal}
-        onCancel={() => setOpenResetPasswordModal(false)}
-      />
       <NavMsgBar className="mr-8" />
       {/* <Button className="mr-32">
           进入应用管理
@@ -43,30 +32,7 @@ export default function HeaderMenu() {
         />
       </div> */}
       <div className="header-nav-btn group">
-        <MoreMenu
-          menus={[
-            { key: 'resetPassword', label: '重置密码' },
-            { key: 'logout', label: '登出' },
-          ]}
-          onMenuClick={(menuKey) => {
-            if (menuKey === 'logout') {
-              window.location.href = '/logout';
-              return;
-            }
-
-            setOpenResetPasswordModal(true);
-          }}
-        >
-          <div
-            className="cursor-pointer flex items-center h-36
-            hover:blue-100 transition group-hover:text-blue-600"
-          >
-            <Avatar
-              username={window.USER.userName}
-            />
-            <Icon name="arrow_drop_down" size={20} />
-          </div>
-        </MoreMenu>
+        <ProfilePicture />
       </div>
     </div>
   );
