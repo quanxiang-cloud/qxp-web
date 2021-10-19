0d8a6f186cadd60c41ce60b7b9c1b93477686d5a
tanjie <starrytan@yunify.com>
2021-10-19 17:11:52 +0800

    fix: Fix process form no data

--- clients/home/pages/approvals/detail/index.tsx
+++ clients/home/pages/approvals/detail/index.tsx
@@ -111,7 +111,7 @@ function ApprovalDetail(): JSX.Element {
       <div className='task-form'>
         <FormRenderer
           value={formData}
-          schema={task.formSchema || {}}
+          schema={(task.formSchema.table || task.formSchema) || {}}
           onFormValueChange={setFormValues}
           readOnly={taskEnd}
           usePermission
