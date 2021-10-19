47052c9501c5f1cd782cb96b321834f743d23cb1
lishengma <lishengma@yunify.com>
2021-10-19 10:37:38 +0800

    fix: Person department datetime readonly default value and subtable support formula

--- clients/components/form-builder/registry/sub-table/preview/index.tsx
+++ clients/components/form-builder/registry/sub-table/preview/index.tsx
@@ -1,14 +1,11 @@
 import React, { JSXElementConstructor, useEffect, useState, useContext } from 'react';
-import { ISchemaFieldComponentProps, IMutators } from '@formily/react-schema-renderer';
 import { Rule } from 'rc-field-form/lib/interface';
 import { Table } from 'antd';
-import { Input, NumberPicker } from '@formily/antd-components';
 import cs from 'classnames';
 import { isObject } from 'lodash';
 import { useQuery } from 'react-query';
-import {
-  InternalFieldList as FieldList, ValidatePatternRules,
-} from '@formily/antd';
+import { InternalFieldList as FieldList, ValidatePatternRules } from '@formily/antd';
+import { ISchemaFieldComponentProps, IMutators } from '@formily/react-schema-renderer';
 
 import CanvasContext from '@c/form-builder/canvas-context';
 import logger from '@lib/logger';
@@ -16,56 +13,29 @@ import FormDataValueRenderer from '@c/form-data-value-renderer';
 import Icon from '@c/icon';
 import { isEmpty } from '@lib/utils';
 import schemaToFields from '@lib/schema-convert';
-import OrganizationPicker from '@c/form-builder/registry/organization-select/organization-select-wrap';
-import FileUpload from '@c/form-builder/registry/file-upload/uploader';
-import ImageUpload from '@c/form-builder/registry/image-upload/uploader';
-import UserPicker from '@c/form-builder/registry/user-picker/user-picker-wrap';
-import CascadeSelector from '@c/form-builder/registry/cascade-selector/cascade-selector-wrap';
-import AssociatedData from '@c/form-builder/registry/associated-data/associated-data';
-import RadioGroup from '@c/form-builder/registry/radio-group/radioGroup';
-import CheckBoxGroup from '@c/form-builder/registry/checkbox-group/checkboxGroup';
-import DatePicker from '@c/form-builder/registry/date-picker/date-picker';
-import Select from '@c/form-builder/registry/select/custom-select';
-import MultipleSelect from '@c/form-builder/registry/multiple-select/multiple-select';
-import Serial from '@c/form-builder/registry/serial-number/serial';
+import { getTableSchema } from '@lib/http-client';
 
 import { getDefaultValue, schemaRulesTransform } from './utils';
 import SubTableRow from './row';
-import { getTableSchema } from '@lib/http-client';
+import { useForeignFormula } from './use-foreign-formula';
+import { components } from './components';
 
 export type Rules = (ValidatePatternRules | ValidatePatternRules[]) & Rule[];
 export type Column = {
   title: string;
   dataIndex: string;
+  componentName: string;
   props: Record<string, any>;
   readOnly: boolean;
   rules: Rules;
   schema: ISchema;
-  component?: JSXElementConstructor<any>;
+  component: JSXElementConstructor<any>;
   dataSource?: any[];
   required?: boolean;
   render?: (value: unknown) => JSX.Element;
 }
 
 type Components = typeof components;
-
-const components = {
-  input: Input,
-  radiogroup: RadioGroup,
-  checkboxgroup: CheckBoxGroup,
-  textarea: Input.TextArea,
-  datepicker: DatePicker,
-  numberpicker: NumberPicker,
-  select: Select,
-  multipleselect: MultipleSelect,
-  userpicker: UserPicker,
-  organizationpicker: OrganizationPicker,
-  fileupload: FileUpload,
-  imageupload: ImageUpload,
-  cascadeselector: CascadeSelector,
-  associateddata: AssociatedData,
-  serial: Serial,
-};
 
 interface SubTableState {
   componentColumns: Column[];
@@ -129,6 +99,8 @@ function SubTable({
     isInitialValueEmpty && mutators?.change([rowPlaceHolder]);
   }, []);
 
+  useForeignFormula(isFromForeign, columns, name, schema);
+
   function buildColumnFromSchema(dataIndex: string, sc: ISchema): Column | null {
     const componentName = sc['x-component']?.toLowerCase() as keyof Components;
     const componentProps = sc['x-component-props'] || {};
@@ -140,6 +112,7 @@ function SubTable({
     }
 
     return {
+      componentName,
       title: sc.title as string,
       dataIndex,
       component: components[componentName],
@@ -148,8 +121,9 @@ function SubTable({
         ...componentPropsInternal,
         'x-component-props': componentProps,
         'x-internal': componentPropsInternal,
+        readOnly: !!sc.readOnly,
       },
-      schema: sc,
+      schema: { ...sc, 'x-internal': { ...sc?.['x-internal'], parentFieldId: name } },
       dataSource,
       required: !!sc?.required,
       readOnly: !!sc.readOnly,
