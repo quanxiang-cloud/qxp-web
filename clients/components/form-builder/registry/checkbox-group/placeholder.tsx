59e52260c70f41fd0ca0c7ce3caf26bb08fdfcc4
luolin <kenluo@yunify.com>
2021-10-20 11:11:45 +0800

    fix: Fix checkbox group align style in form builder

--- clients/components/form-builder/registry/checkbox-group/placeholder.tsx
+++ clients/components/form-builder/registry/checkbox-group/placeholder.tsx
@@ -5,15 +5,34 @@ import { ISchemaFieldComponentProps } from '@formily/antd';
 import useEnumOptions from '@lib/hooks/use-enum-options';
 
 const CheckboxGroup = Checkbox.Group;
+function CheckBoxGroup({
+  options = [],
+  optionsLayout = '',
+}: {
+  optionsLayout: string;
+  options: string[];
+}): JSX.Element {
+  if (optionsLayout === 'vertical') {
+    return (
+      <div className='flex flex-col'>
+        {options.map((ck: string) =>
+          (<span key={ck} className='inline-block'><Checkbox>{ck}</Checkbox></span>))
+        }
+      </div>
+    );
+  }
+
+  return <CheckboxGroup options={options} />;
+}
 
 function Placeholder(fieldProps: ISchemaFieldComponentProps): JSX.Element {
   const labels = useEnumOptions(fieldProps);
+  const { optionsLayout } = fieldProps?.props?.['x-component-props'];
 
-  return (
-    <>
-      {!labels.length ? <span>暂无可选项</span> : <CheckboxGroup options={labels} />}
-    </>
-  );
+  if (!labels.length) {
+    return <span>暂无可选项</span>;
+  }
+  return <CheckBoxGroup optionsLayout={optionsLayout} options={labels} />;
 }
 
 export default Placeholder;
