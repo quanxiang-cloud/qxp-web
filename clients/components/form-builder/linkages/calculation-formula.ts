d7699f666712f18868268ec6f936d2036e8aec87
lishengma <lishengma@yunify.com>
2021-10-22 14:13:22 +0800

    fix: Formula caculation

--- clients/components/form-builder/linkages/calculation-formula.ts
+++ clients/components/form-builder/linkages/calculation-formula.ts
@@ -1,9 +1,10 @@
-import { get } from 'lodash';
-import { of } from 'rxjs';
-import { switchMap, debounceTime, filter } from 'rxjs/operators';
+import { get, isNull } from 'lodash';
+import { of, Observable } from 'rxjs';
+import { switchMap, debounceTime, filter, reduce, map as mapOp } from 'rxjs/operators';
 import { FormEffectHooks, ISchemaFormActions, IFieldState } from '@formily/antd';
-import { flattenDeep, last, isArray } from 'lodash';
-import { findVariables, parse, resolve } from 'qxp-formula';
+import { flattenDeep, isArray, isEmpty, isUndefined } from 'lodash';
+import { findVariables, LogicalFormula, parse, resolve } from 'qxp-formula';
+import { pipe, map, ifElse, pick, last, fromPairs } from 'ramda';
 
 import logger from '@lib/logger';
 import { getFieldPath } from '@c/form-builder/linkages/default-value';
@@ -39,73 +40,114 @@ type ExecuteFormulaProps = {
   formActions: ISchemaFormActions,
 }
 
+type DependentFieldsValue = {
+  values: Record<string, any>;
+  missingValueField: boolean;
+}
+
 function executeFormula({ rawFormula, formActions, targetField }: ExecuteFormulaProps): void {
   const { setFieldState, getFieldValue } = formActions;
   const ast = parse(rawFormula);
-  let dependentFields = [...findVariables(ast)];
+
+  const getDependentFields = pipe<LogicalFormula, Set<string>, string[], string[]>(
+    () => ast,
+    findVariables,
+    (variables) => [...variables],
+    map((dependentField: string) => dependentField.replace('_dot_star_dot_', '.*.')),
+  );
+  const dependentFields = getDependentFields();
+  const subTableDependtFields = dependentFields
+    .filter((field) => field.includes('.*.'))
+    .map((field) => last(field.split('.*.')))
+    .filter(Boolean) as string[];
+
   if (!dependentFields.length) {
     logger.warn('skip execution for formula: [%{rawFormula}], due to no dependentFields found.');
     return;
   }
-  dependentFields = dependentFields.map((dependentField) => {
-    const prefix = targetField.split('.').slice(0, -1).join('.');
-    if (!dependentField.startsWith('subtable_')) {
-      return dependentField;
-    }
-    return `${prefix}.${dependentField}`;
-  });
+
+  function dependentFieldsValueReducerGenerator(state: IFieldState): (
+    acc: DependentFieldsValue, current: string
+  ) => DependentFieldsValue {
+    let missingValueField = false;
+    return (valuesAcc: DependentFieldsValue, fieldName: string) => {
+      let fieldPath = getFieldPath(fieldName, state.path);
+      if (fieldPath.includes('.*.')) {
+        fieldPath = fieldPath.split('.*.').slice(0, -1).join('.');
+      }
+      const fieldValue = getFieldValue(fieldPath);
+      const fieldValueFilter = (val: Record<string, any>[] | string): Record<string, any> | string => {
+        if (!isArray(val)) {
+          return val;
+        }
+        return val.filter((value) => Object.values(pick(subTableDependtFields, value)).every(
+          (value) => !isUndefined(value),
+        ));
+      };
+      const condition = ifElse(
+        (val) => isUndefined(val) || isNull(val) || (isArray(val) && val.every(isEmpty)),
+        () => missingValueField = true,
+        () => valuesAcc.values[fieldPath] = fieldValueFilter(fieldValue),
+      );
+      condition(fieldValue);
+      return { ...valuesAcc, missingValueField };
+    };
+  }
+
+  function combineOperator(state: IFieldState) {
+    return (obs$: Observable<any>) => new Observable((observer) => obs$.subscribe({
+      next: (value: { values: Record<string, any>, missingValueField: boolean }) => {
+        return observer.next([value.missingValueField, value.values, state]);
+      },
+      error: (err: Error) => observer.error(err),
+      complete: () => observer.complete(),
+    }));
+  }
 
   // *(abc,def,gij)
   onFieldValueChange$(`*(${dependentFields.join(',')})`).pipe(
     debounceTime(200),
-    switchMap((state) => {
-      let missingValueField = false;
-      const values = dependentFields.reduce<{ [key: string]: any }>((acc, fieldName) => {
-        let path = getFieldPath(fieldName, state.path);
-        path = path.includes('.*.') ? path.split('.*.').slice(0, -1).join('.') : path;
-        const value = getFieldValue(path);
-        if (value === undefined || value === null) {
-          missingValueField = true;
-        }
-        const lastLevelFieldName = last(fieldName.split('.'));
-        if (lastLevelFieldName) {
-          acc[lastLevelFieldName] = value;
-        }
-        return acc;
-      }, {});
-      return of([missingValueField, values, state]);
-    }),
-    filter((input): input is [true, Record<string, any>, IFieldState<any>] => {
+    switchMap((state) => of(...dependentFields).pipe(
+      reduce(dependentFieldsValueReducerGenerator(state), { values: {}, missingValueField: false }),
+      combineOperator(state),
+    )),
+    filter((input: any): input is [true, Record<string, any>, IFieldState<any>] => {
       const [missingValueField] = input;
-      if (missingValueField) {
-        return false;
-      }
-
-      return true;
+      return !missingValueField;
     }),
-  ).subscribe(([, values, state]) => {
-    logger.debug('execute calculation formula on form field', 'todo');
-    try {
-      const arrayKey = Object.keys(values).find((key) => isArray(values[key])) || '';
-      const arrayValue = values[arrayKey];
-      if (isArray(arrayValue)) {
-        return arrayValue.forEach((val: any, index: number) => {
-          const currentValues = { ...values, [arrayKey]: val[arrayKey] };
-          const value = resolve(ast, currentValues);
-          setFieldState(
-            getFieldPath(targetField, targetField.replace('.*.', `.${index}.`)),
-            (state) => state.value = value,
-          );
-        });
+    mapOp(([, values, state]) => [values, state.path]),
+  ).subscribe(([_values, _currentPath]) => {
+    const values = _values as Record<string, any>;
+    const currentPath = _currentPath as string;
+    let lineNumber: number | undefined = Number(currentPath.match(/\.(\d+)\./)?.[1]);
+    lineNumber = isNaN(lineNumber) ? undefined : lineNumber;
+    const subTableFieldKey = lineNumber ? undefined : Object.keys(values).find((key) => isArray(values[key]));
+    function assignValue(values: Record<string, any>, path: string): void {
+      try {
+        const resultValue = resolve(ast, values);
+        setFieldState(path, (state) => state.value = resultValue);
+      } catch (err: any) {
+        logger.debug('execute calculation formula on form field', 'todo');
       }
-      const value = resolve(ast, values);
-      setFieldState(
-        getFieldPath(targetField, state.path),
-        (state) => state.value = value,
-      );
-    } catch (err) {
-      logger.error('failed to calculate value from formula', err);
     }
+    function subTableValueConvertor(value: Record<string, any>, prefix?: string): Record<string, any> {
+      return fromPairs(Object.entries(value).map(([key, value]) => {
+        return [prefix ? `${prefix}_dot_star_dot_${key}` : key.replace(/\.\d+\./, '_dot_star_dot_'), value];
+      }));
+    }
+    function processMainTableChange(): void {
+      values[subTableFieldKey as string].forEach((value: Record<string, any>, lineNumber: number) => {
+        assignValue(
+          { ...values, ...subTableValueConvertor(value, subTableFieldKey) },
+          targetField.replace('.*.', `.${lineNumber}.`),
+        );
+      });
+    }
+    function processNormalOrSubTableChange(): void {
+      assignValue(subTableValueConvertor(values), getFieldPath(targetField, currentPath));
+    }
+    const condition = ifElse(Boolean, processMainTableChange, processNormalOrSubTableChange);
+    condition(subTableFieldKey);
   });
 }
 
