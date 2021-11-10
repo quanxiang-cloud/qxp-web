d27dfaa733257f1815a51925286314a74655f076
Bunny <qimiao@yunify.com>
2021-11-10 15:19:25 +0800

    refactor: Add content for request drawer

--- clients/portal/modules/poly-api/containers/node-config.tsx
+++ clients/portal/modules/poly-api/containers/node-config.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { SchemaForm } from '@formily/react-schema-renderer';
 import { isEmpty } from 'ramda';
-import { Input } from '@formily/antd-components';
 
+import Select from '@c/select';
 import Drawer from '@c/drawer';
 import useObservable from '@lib/hooks/use-observable';
 
@@ -11,7 +10,7 @@ import store$ from '../store';
 export default function NodeConfigDrawer(): JSX.Element {
   const store = useObservable(store$);
   const initialConfigParams = {
-    currentNodeConfigParams: { schema: {}, currentNode: undefined, onClose: undefined },
+    currentNodeConfigParams: { schema: {}, currentNode: undefined, onClose: undefined, full: false },
   };
   const {
     currentNodeConfigParams: { schema, currentNode, onClose },
@@ -23,24 +22,50 @@ export default function NodeConfigDrawer(): JSX.Element {
       schema: {},
       currentNode: undefined,
       onClose: undefined,
+      full: false,
     });
   }
 
   return (
     <Drawer
-      title={currentNode?.value.title || ''}
+      position="right"
+      className="node-config-drawer"
+      title={`配置${currentNode?.value.type}节点`}
       onCancel={onCancel}
       visible={!!(schema && currentNode)}
-      position="right"
     >
-      {!isEmpty(schema) && (
-        <SchemaForm
-          schema={schema}
-          components={{ input: Input }}
-          defaultValue={currentNode?.value?.detail}
-          onChange={(value) => currentNode?.set('detail', value)}
-        />
-      )}
+      <div className="px-20 py-12 flex items-center justify-between border-b-1">
+        <div className="flex items-center mr-16 flex-1">
+          <span>全部API：</span>
+          <Select className="flex-1" options={[]} />
+        </div>
+        <div className="flex items-center justify-between flex-2">
+          <span>请求方法：</span>
+          <span className="text-green-600 mr-8">POST</span>
+          <span>接口路径：</span>
+          <span className="mr-8 flex-1 truncate">http://www.qingcloud.com.sdasdasfasfasdasd..</span>
+          <span>API标识：</span>
+          <span>ap</span>
+        </div>
+      </div>
+      <div className="flex">
+        <div className="p-12 flex-2">
+
+        </div>
+        <div className="flex-1 px-12 border-l-1 border-gray-200 flex flex-col">
+          <div className="flex items-center py-12">
+            <span className="text-12 font-semibold">运算符：</span>
+            <div className="flex-1 flex items-center justify-between">
+              <span className="request-op">+</span>
+              <span className="request-op">-</span>
+              <span className="request-op">/</span>
+              <span className="request-op">*</span>
+              <span className="request-op">()</span>
+            </div>
+          </div>
+          <div className="pt-12 border-t-1 border-gray-200 flex-1">DataTree</div>
+        </div>
+      </div>
     </Drawer>
   );
 }
