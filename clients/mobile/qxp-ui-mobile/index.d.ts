33663608f4cb5ed88542acdc49b0317e678e1dfa
chengww <chengww5217@163.com>
2021-11-30 10:48:16 +0800

    style: Update qxp ui mobile props

--- clients/mobile/qxp-ui-mobile/index.d.ts
+++ clients/mobile/qxp-ui-mobile/index.d.ts
@@ -1,8 +1,16 @@
-import { CSSProperties } from 'react';
+import React, { CSSProperties, ReactNode } from 'react';
 
 export interface Props {
-  className?: string,
-  style?: CSSProperties
+  className?: string;
+  style?: CSSProperties;
+  onClick?: (e: React.MouseEvent) => void;
+  children?: ReactNode;
 }
 
-type RenderFunc = () => JSX.Element;
+export type RenderFunc = () => React.ReactNode;
+
+export type NumberString = number | string;
+
+export type VoidCallback = () => void;
+
+export type TeleportType = HTMLElement | (() => HTMLElement) | null;
