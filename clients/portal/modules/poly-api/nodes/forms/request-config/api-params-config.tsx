f48a4c353d97761fdf64b13a6168ccd7953c93c1
Bunny <qimiao@yunify.com>
2021-12-01 15:40:30 +0800

    refactor: Add $ for treenode path

--- clients/portal/modules/poly-api/nodes/forms/request-config/api-params-config.tsx
+++ clients/portal/modules/poly-api/nodes/forms/request-config/api-params-config.tsx
@@ -22,6 +22,7 @@ function ApiParamsConfig(
   { value, onChange, customRules, url }: Props,
   ref: ForwardedRef<RefType | undefined>,
 ): JSX.Element {
+  console.log(customRules);
   const formulaRefs = useRef<Record<string, RefProps>>({});
   const currentFormulaEditorRef = useRef<RefProps | null>(null);
   const errorsRef = useRef<Record<string, string>>({});
