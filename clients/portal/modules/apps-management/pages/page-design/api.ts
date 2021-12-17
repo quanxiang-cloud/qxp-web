33e40c0953e4b83e0f7be77c8a62031a21f63b0e
sunnyw <sunnywang@yunify.com>
2021-12-17 17:04:46 +0800

    chore: Page engine save and preview

--- clients/portal/modules/apps-management/pages/page-design/api.ts
+++ clients/portal/modules/apps-management/pages/page-design/api.ts
@@ -7,27 +7,27 @@ import toast from '@lib/toast';
 
 export const PG_SAVED_PREFIX = 'pge-';
 export const PG_DRAFT_PREFIX = 'pge-draft-';
-export const PG_VERSION = 'v0.1.0';
+export const PG_VERSION = '0.1.0';
 
 type Option={
   draft?: boolean;
   [key: string]: any
 }
 
-function getPageQueryId(app_id: string, page_id: string, isDraft?: boolean) {
+export function getSchemaKey(app_id: string, page_id: string, isDraft?: boolean) {
   return [isDraft ? PG_DRAFT_PREFIX : PG_SAVED_PREFIX, [app_id, page_id].join('__')].join('@');
 }
 
 export function savePage(app_id: string, page_id: string, page_schema: any, options?: Option): Promise<any> {
   return setBatchGlobalConfig([{
-    key: getPageQueryId(app_id, page_id, options?.draft),
+    key: getSchemaKey(app_id, page_id, options?.draft),
     version: PG_VERSION,
     value: typeof page_schema === 'object' ? JSON.stringify(page_schema) : page_schema,
   }]);
 }
 
 export function getPage(app_id: string, page_id: string, options?: Option) {
-  const queryId = getPageQueryId(app_id, page_id, options?.draft);
+  const queryId = getSchemaKey(app_id, page_id, options?.draft);
   return getBatchGlobalConfig([{
     key: queryId,
     version: PG_VERSION,
@@ -36,4 +36,8 @@ export function getPage(app_id: string, page_id: string, options?: Option) {
       return result[queryId];
     })
     .catch((err)=> toast.error(err.message));
+}
+
+export function getVersionKey(): string {
+  return PG_VERSION;
 }
--- clients/portal/modules/apps-management/pages/page-design/page-preview/index.tsx
+++ clients/portal/modules/apps-management/pages/page-design/page-preview/index.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+
+import PageSchemaRender from '@c/page-schema-render';
+import { getStore } from '@ofa/page-engine';
+import { getSchemaKey, getVersionKey } from '../api';
 
 interface Props {
   className?: string;
 }
 
 function PagePreview(props: Props) {
+  const { appID, pageId } = useParams<{appID: string, pageId: string}>();
+  const pageCtx = getStore();
+  const repository = {
+    'ofa-ui@latest': pageCtx.registry.toComponentMap(),
+  };
+
   return (
-    <div>
-      page preview
-    </div>
+    <PageSchemaRender
+      schemaKey={getSchemaKey(appID, pageId, true)}
+      version={getVersionKey()}
+      repository={repository}
+    />
   );
 }
 
