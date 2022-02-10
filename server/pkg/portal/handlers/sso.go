c9451b7e0757fce5208fa27911fec6886901c320
Mark <markduan@yunify.com>
2022-02-10 16:14:07 +0800

    refactor: Optimize sso login auth handler

--- server/pkg/portal/handlers/sso.go
+++ server/pkg/portal/handlers/sso.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"qxp-web/server/pkg/contexts"
 	"time"
@@ -11,44 +12,61 @@ import (
 func HandleSSOLogin(w http.ResponseWriter, r *http.Request) {
 	requestID := contexts.GetRequestID(r)
 	code := r.URL.Query().Get("code")
-	url := r.URL.Query().Get("redirect_url")
+	redirectURL := r.URL.Query().Get("redirect_url")
+	if redirectURL == "" {
+		redirectURL = "/"
+	}
 
 	if code == "" {
 		contexts.Logger.Errorf("sso login failed, err: code is empty, request_id: %s", requestID)
 		w.Write([]byte("code is required"))
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	resBody, err := contexts.SendRequest(r.Context(), "POST", "/api/v1/jwt/auth", []byte(""), map[string]string{
+	resBody, errMsg := contexts.SendRequest(r.Context(), "POST", "/api/v1/jwt/auth", []byte(""), map[string]string{
 		"Content-Type": "application/json",
 		"Access-Token": code,
 	})
-	if err != "" {
-		contexts.Logger.Errorf("sso login failed, err: %s, request_id: %s", err, requestID)
-		w.Write([]byte("login failed"))
+
+	if errMsg != "" {
+		message := fmt.Sprintf("sso login failed, err: %s, request_id: %s", errMsg, requestID)
+		contexts.Logger.Error(message)
+
+		w.Write([]byte(message))
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
 	var loginResponse LoginResponse
 	if err := json.Unmarshal(resBody, &loginResponse); err != nil {
-		contexts.Logger.Errorf("sso login failed, err: %s, request_id: %s", err.Error(), requestID)
-		w.Write([]byte("login failed"))
+		contexts.Logger.Errorf("failed to unmarshal login response: %s, request_id: %s", err.Error(), requestID)
+		w.Write([]byte("sso login failed due to internal error occurred, please contact administrator."))
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
 	if loginResponse.Code != 0 {
-		contexts.Logger.Errorf("sso login failed, err: %s, request_id: %s", loginResponse.Message, requestID)
-		w.Write([]byte("login failed"))
+		message := fmt.Sprintf("sso login failed, err: %s, request_id: %s", loginResponse.Message, requestID)
+		contexts.Logger.Error(message)
+
+		w.Write([]byte(message))
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	if expireTime, err := time.Parse(time.RFC3339, loginResponse.Data.Expire); err == nil {
-		saveToken(r, loginResponse.Data.AccessToken, loginResponse.Data.RefreshToken, expireTime)
-		contexts.Logger.Infof("sso login success, redirect to %s, request_id: %s", url, requestID)
-		http.Redirect(w, r, url, http.StatusFound)
-	} else {
-		contexts.Logger.Errorf("sso login failed, err: %s, request_id: %s", err.Error(), requestID)
-		w.Write([]byte("login failed"))
+	expireTime, err := time.Parse(time.RFC3339, loginResponse.Data.Expire)
+	if err != nil {
+		message := fmt.Sprintf("failed to parse login response: %s, request_id: %s", err.Error(), requestID)
+		contexts.Logger.Errorf(message)
+
+		w.Write([]byte(message))
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
+
+	saveToken(r, loginResponse.Data.AccessToken, loginResponse.Data.RefreshToken, expireTime)
+	contexts.Logger.Infof("sso login success, redirect user to %s, request_id: %s", redirectURL, requestID)
+
+	http.Redirect(w, r, redirectURL, http.StatusFound)
 }
