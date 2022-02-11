33704d738984e05debeccaa6456f226dbcd8e06c
sunnyw <sunnywang@yunify.com>
2022-02-11 16:52:50 +0800

    chore: Update page-engine, ui version

--- clients/components/page-schema-render/index.tsx
+++ clients/components/page-schema-render/index.tsx
@@ -22,7 +22,10 @@ export default function PageSchemaRender(
 
   return (
     <ErrorBoundary>
-      <div className="overflow-auto relative z-0" style={{ maxHeight: maxHeight || '100vh' }}>
+      <div
+        className="overflow-auto relative z-0"
+        style={{ maxHeight: maxHeight || '100vh', height: '100vh' }}
+      >
         <SchemaRender schema={schema} apiSpecAdapter={adapter} repository={repository} />
       </div>
     </ErrorBoundary>
