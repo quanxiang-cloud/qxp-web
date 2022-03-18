ab0ffc702529a1c5c792a1379559e6999732c6d2
Mark <markduan@yunify.com>
2022-03-18 18:38:03 +0800

    refactor: Optimize SchemaRender

--- clients/app-land/app-land.tsx
+++ clients/app-land/app-land.tsx
@@ -1,36 +1,12 @@
 import React from 'react';
-import type { Repository } from '@one-for-all/render-engine';
-import * as OneForAllUI from '@one-for-all/ui';
-import { QueryClient, QueryClientProvider } from 'react-query';
 
 import { VERSION } from '@portal/modules/apps-management/pages/app-details/view-orchestration/constants';
+import SchemaRender from '@c/page-schema-render';
 
-import SchemaRender from './page-schema-render';
 import { rootSchemaKey } from './utils';
-
-// todo fix this
-const repository: Repository = {
-  // @ts-ignore
-  'ofa-ui@latest': OneForAllUI,
-};
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      retry: false,
-    },
-  },
-});
 
 export default function AppLand(): JSX.Element {
   return (
-    <QueryClientProvider client={queryClient}>
-      <SchemaRender
-        schemaKey={rootSchemaKey}
-        version={VERSION}
-        repository={repository}
-      />
-    </QueryClientProvider>
+    <SchemaRender schemaKey={rootSchemaKey} version={VERSION} />
   );
 }
