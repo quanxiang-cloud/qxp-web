5e02b6f26d1fe90050ea38613e68fafb9243b024
eli <siminchen@yunify.com>
2022-03-22 18:06:57 +0800

    fix: Fix placeholder problem

--- clients/components/form-builder/registry/date-picker/placeholder.tsx
+++ clients/components/form-builder/registry/date-picker/placeholder.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { ISchemaFieldComponentProps } from '@formily/antd';
 
 import Icon from '@c/icon';
 
 import './index.scss';
 
-function Placeholder(): JSX.Element {
+function Placeholder({ props }: ISchemaFieldComponentProps): JSX.Element {
+  const placeholder = props?.['x-component-props']?.placeholder;
   return (
     <div className='placeholder-date-picker'>
       <Icon name='calendar_today' className='date-icon' />
-      <span className='ml-10 text-gray-300'>请选择日期</span>
+      <span className='ml-10 text-gray-300'>{placeholder || '请输入'}</span>
     </div>
   );
 }
