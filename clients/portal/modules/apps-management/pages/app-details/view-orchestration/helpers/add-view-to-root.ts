2658770d63f88254d7e5d355da110ee4c4516315
tanjie <starrytan@yunify.com>
2022-03-24 19:44:06 +0800

    chore: Reset code

--- clients/portal/modules/apps-management/pages/app-details/view-orchestration/helpers/add-view-to-root.ts
+++
@@ -1,7 +0,0 @@
-import { SchemaNode } from '@one-for-all/schema-spec';
-import { addRouteNodeToRootNode, attachToRouteNode } from './utils';
-
-export default function addViewToRoot(rootNode: SchemaNode, view: SchemaNode): SchemaNode | undefined {
-  const routeNode = attachToRouteNode(view, 'view');
-  return addRouteNodeToRootNode(rootNode, routeNode);
-}
