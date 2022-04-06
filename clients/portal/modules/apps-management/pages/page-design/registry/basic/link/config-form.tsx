c8a27a3e15836029f659ffb57363cc37c3338201
Bunny miao <49241072+hhhqm@users.noreply.github.com>
2022-04-06 18:57:58 +0800

    refactor: Add placeholder for link url input (#27)
    * refactor: Add placeholder for link url input
    
    * refactor: Update link input placeholder text
    
    Co-authored-by: Bunny <qimiao@yunify.com>
    Co-authored-by: Mark <markduan@yunify.com>

--- clients/portal/modules/apps-management/pages/page-design/registry/basic/link/config-form.tsx
+++ clients/portal/modules/apps-management/pages/page-design/registry/basic/link/config-form.tsx
@@ -78,20 +78,7 @@ function ConfigForm(): JSX.Element {
               onChange={(linkPage) => setValues({ ...values, linkPage })}
             />
           </div>
-        ) : ( */}
-      <div className='mb-8'>
-        <p className='mr-4 text-12 text-gray-600'>链接地址</p>
-        <div className='flex items-center'>
-          <input
-            className='w-full h-32 my-4 px-8 mr-8 border
-                corner-2-8-8-8 border-gray-300 focus:border-blue-600'
-            value={values.linkUrl}
-            onChange={(ev) => setValues({ ...values, linkUrl: ev.target.value })}
-          />
-          <ConfigBind name='linkUrl' />
-        </div>
-      </div>
-      {/* )
+        ) : (<></>)
       } */}
       <div className='mb-8'>
         <p className='text-12 text-gray-600'>链接类型</p>
@@ -111,6 +98,18 @@ function ConfigForm(): JSX.Element {
             defaultChecked={values.linkType === 'inside'}
           />
         </RadioGroup>
+      </div>
+      <div className='mb-8'>
+        <p className='mr-4 text-12 text-gray-600'>链接地址</p>
+        <div className='flex items-center'>
+          <input
+            placeholder={values.linkType === 'outside' ? '请输入完整url路径 例：https://example.com' : '请输入路由path 例：/a/path/to/some/page'}
+            className='w-full h-32 my-4 px-8 mr-8 border corner-2-8-8-8 border-gray-300 focus:border-blue-600'
+            value={values.linkUrl}
+            onChange={(ev) => setValues({ ...values, linkUrl: ev.target.value })}
+          />
+          <ConfigBind name='linkUrl' />
+        </div>
       </div>
       <div className='mb-8 flex items-center justify-between'>
         <div className='w-full flex items-center'>
