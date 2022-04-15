cefb0d106ed8e437e1f827ecdc3cabc721f74d2b
songwei <songwei@yunify.com>
2022-04-15 18:08:11 +0800

    fix: Change option set api

--- clients/portal/modules/option-set/api.ts
+++ clients/portal/modules/option-set/api.ts
@@ -1,22 +1,22 @@
 import httpClient from '@lib/http-client';
 
 export function getOptionSetNames(params?: OptionSets): Promise<{ list: OptionSet[] }> {
-  return httpClient('/api/v1/form/dataset/m/getByCondition', params || {});
+  return httpClient('/api/v1/persona/dataset/m/getByCondition', params || {});
 }
 
 export function getOptionSetById(id: string): Promise<OptionSet> {
   const side = window.SIDE === 'home' ? 'home' : 'm';
-  return httpClient(`/api/v1/form/dataset/${side}/get`, { id });
+  return httpClient(`/api/v1/persona/dataset/${side}/get`, { id });
 }
 
 export function createOptionSet(dataset: Omit<OptionSet, 'id'>): Promise<{ id: string }> {
-  return httpClient('/api/v1/form/dataset/m/create', dataset);
+  return httpClient('/api/v1/persona/dataset/m/create', dataset);
 }
 
 export function updateOptionSet(dataset: OptionSet): Promise<{ id?: string }> {
-  return httpClient('/api/v1/form/dataset/m/update', dataset);
+  return httpClient('/api/v1/persona/dataset/m/update', dataset);
 }
 
 export function deleteOptionSet(id: string): Promise<{ id?: string }> {
-  return httpClient('/api/v1/form/dataset/m/delete', { id });
+  return httpClient('/api/v1/persona/dataset/m/delete', { id });
 }
