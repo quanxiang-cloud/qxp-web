a2d98abab17ffdeca8d216f4319b5d1fba917bc9
eli <siminchen@yunify.com>
2022-04-20 13:26:52 +0800

    feat: Allow scss and save scss to personal

--- clients/portal/modules/style-guide/component-style-customization/css-editor.tsx
+++ clients/portal/modules/style-guide/component-style-customization/css-editor.tsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { observer } from 'mobx-react';
-import '@one-for-all/style-guide-csslint';
+// import '@one-for-all/style-guide-csslint';
 
-import toast from '@lib/toast';
+// import toast from '@lib/toast';
 
-import { applyStyle } from '../utils';
+// import { applyStyle } from '../utils';
 import store from '../store';
 
 const CodeMirror = React.lazy(() => import('./codemirror'));
@@ -14,10 +14,16 @@ function CSSEditor(): JSX.Element {
   const [isFocus, setFocus] = useState(false);
   const [isChanged, setIsChanged] = useState(false);
   const { spec, key } = store.currentCompStatus as ActiveConfigurationComponent;
-
   useEffect(() => {
-    setValue(store.cssStore?.getInitCompCss(`${key}.${spec.title}`, spec.rules) || '');
-  }, [store.currentCompStatus]);
+    store.fetchComponentScss(`${key}.${spec.title}`);
+  }, []);
+  useEffect(() => {
+    if (store.componentScssMap[`${key}.${spec.title}`]) {
+      setValue(store.componentScssMap[`${key}.${spec.title}`]);
+    } else {
+      setValue(store.cssStore?.getInitCompCss(`${key}.${spec.title}`, spec.rules) || '');
+    }
+  }, [store.currentCompStatus, store.componentScssMap]);
 
   useEffect(() => {
     if (isFocus) {
@@ -31,9 +37,11 @@ function CSSEditor(): JSX.Element {
       return;
     }
 
-    store.cssStore?.setCss(`${key}.${spec.title}`, value, spec.rules, (msg) => toast.error(msg));
-    const css = store.cssStore?.getCssString();
-    store.shadowRoot && applyStyle(css || '', store.shadowRoot);
+    store.updateComponentScssMap(`${key}.${spec.title}`, value);
+    store.setComponentScss(`${key}.${spec.title}`, value);
+    // store.cssStore?.setCss(`${key}.${spec.title}`, value, spec.rules, (msg) => toast.error(msg));
+    // const css = store.cssStore?.getCssString();
+    // store.shadowRoot && applyStyle(css || '', store.shadowRoot);
     setIsChanged(false);
   }
 
@@ -46,10 +54,9 @@ function CSSEditor(): JSX.Element {
           onFocus={() => setFocus(true)}
           onBlur={() => setFocus(false)}
           options={{
-            mode: 'css',
+            mode: 'text/x-scss',
             theme: 'material',
             lineNumbers: true,
-            lint: true,
             gutters: ['CodeMirror-lint-markers'],
             extraKeys: {
               Alt: 'autocomplete',
--- clients/portal/modules/style-guide/store.ts
+++ clients/portal/modules/style-guide/store.ts
@@ -1,11 +1,17 @@
 import { observable, action, reaction, IReactionDisposer } from 'mobx';
 import { nanoid } from 'nanoid';
-import CssASTStore, { ComponentSpec, StyleConfigInterface } from '@one-for-all/style-guide';
+import CssASTStore, {
+  ComponentSpec,
+  StyleConfigInterface,
+} from '@one-for-all/style-guide';
 import componentSpecs from '@one-for-all/headless-ui-interfaces';
 
 import httpClient from '@lib/http-client';
 import toast from '@lib/toast';
-import { setBatchGlobalConfig, getBatchGlobalConfig } from '@lib/api/user-config';
+import {
+  setBatchGlobalConfig,
+  getBatchGlobalConfig,
+} from '@lib/api/user-config';
 import { parseJSON } from '@lib/utils';
 
 import colorVars from './css-variables.json';
@@ -13,9 +19,9 @@ import { applyStyle } from './utils';
 import DesignTokenStore from './design-token/store';
 
 const COMPONENT_STYLE_CONFIG_KEY = 'GLOBAL_COMPONENT_STYLE_CONFIG';
-const BASE_STYLE_CONFIG_KEY = 'GLOBAL_BASE_STYLE_CONFIG';
-const PERSONALIZED_CONFIG_KEY = 'PERSONALIZED_CONFIG';
-const DESIGN_TOKEN_CONFIG_KEY = 'DESIGN_TOKEN_CONFIG';
+const COMPONENT_STYLE_SCSS_KEY = 'GLOBAL_COMPONENT_STYLE_SCSS';
+const DESIGN_TOKEN_CONFIG_KEY = 'GLOBAL_DESIGN_TOKEN_CONFIG';
+const DESIGN_TOKEN_SCSS_KEY = 'GLOBAL_DESIGN_TOKEN_SCSS';
 const VERSION = '0.1.0';
 class StyleGuideStore {
   destroySetShadowStyle: IReactionDisposer;
@@ -25,15 +31,27 @@ class StyleGuideStore {
   @observable currentComp: ComponentSpec | null = null;
   @observable commonConfig: StyleGuideCommonConfig = { primaryColor: 'blue' };
   @observable shadowRoot: ShadowRoot | null = null;
+  @observable componentScssMap: Record<string, string> = {};
 
   constructor() {
-    this.destroySetShadowStyle = reaction(() => this.shadowRoot, (shadowRoot) => {
-      if (!shadowRoot) {
-        return;
-      }
+    this.destroySetShadowStyle = reaction(
+      () => this.shadowRoot,
+      (shadowRoot) => {
+        if (!shadowRoot) {
+          return;
+        }
 
-      applyStyle(this.cssStore?.getCssString() || '', shadowRoot);
-    });
+        applyStyle(this.cssStore?.getCssString() || '', shadowRoot);
+      },
+    );
+  }
+
+  @action
+  updateComponentScssMap(key: string, css: string): void {
+    this.componentScssMap = {
+      ...this.componentScssMap,
+      [key]: css,
+    };
   }
 
   @action
@@ -51,8 +69,19 @@ class StyleGuideStore {
 
   @action
   fetchStyleConfig = (): void => {
-    getBatchGlobalConfig([{ key: COMPONENT_STYLE_CONFIG_KEY, version: VERSION }]).then((res) => {
-      const customCompCssMap = parseJSON(res.result?.[COMPONENT_STYLE_CONFIG_KEY], {});
+    getBatchGlobalConfig([
+      { key: COMPONENT_STYLE_CONFIG_KEY, version: VERSION },
+      { key: COMPONENT_STYLE_SCSS_KEY, version: VERSION },
+    ]).then((res) => {
+      const customCompCssMap = parseJSON(
+        res.result?.[COMPONENT_STYLE_CONFIG_KEY],
+        {},
+      );
+      const componentsScssMap = parseJSON(
+        res.result?.[COMPONENT_STYLE_SCSS_KEY],
+        {},
+      );
+      this.componentScssMap = componentsScssMap;
       this.cssStore = new CssASTStore({
         initCssMap: customCompCssMap,
         baseColorVariables: colorVars.baseColors,
@@ -63,7 +92,9 @@ class StyleGuideStore {
 
   @action
   fetchDesignTokenConfig = (): void => {
-    getBatchGlobalConfig([{ key: DESIGN_TOKEN_CONFIG_KEY, version: VERSION }]).then((res) => {
+    getBatchGlobalConfig([
+      { key: DESIGN_TOKEN_CONFIG_KEY, version: VERSION },
+    ]).then((res) => {
       this.designTokenStore = new DesignTokenStore({
         tokenData: res.result?.[DESIGN_TOKEN_CONFIG_KEY],
       });
@@ -82,25 +113,45 @@ class StyleGuideStore {
   };
 
   @action
+  fetchComponentScss = async (key: string): Promise<void> => {
+    getBatchGlobalConfig([{
+      key: key, version: VERSION,
+    }]).then((res) => {
+      this.updateComponentScssMap(key, res.result?.[key]);
+    });
+  };
+
+  @action
+  setComponentScss = async (key: string, cssString: string): Promise<void> => {
+    this.updateComponentScssMap(key, cssString);
+    await setBatchGlobalConfig([
+      {
+        version: VERSION,
+        key: key,
+        value: cssString,
+      },
+    ]);
+  };
+
+  @action
   saveStyleConfig = async (): Promise<void> => {
-    const styleCssUrl = await this.generateCssUrl();
-    await setBatchGlobalConfig([{
-      version: VERSION,
-      key: COMPONENT_STYLE_CONFIG_KEY,
-      value: JSON.stringify(this.cssStore?.cssASTMap),
-    }, {
-      version: VERSION,
-      key: PERSONALIZED_CONFIG_KEY,
-      value: JSON.stringify({ styleCssUrl }),
-    }, {
-      version: VERSION,
-      key: DESIGN_TOKEN_CONFIG_KEY,
-      value: JSON.stringify(JSON.parse(this.designTokenStore?.getAllStringTokens() || '')),
-    }, {
-      version: VERSION,
-      key: BASE_STYLE_CONFIG_KEY,
-      value: this.designTokenStore?.generateCssString() || '',
-    }]);
+    await setBatchGlobalConfig([
+      {
+        version: VERSION,
+        key: COMPONENT_STYLE_CONFIG_KEY,
+        value: JSON.stringify(this.cssStore?.cssASTMap),
+      },
+      {
+        version: VERSION,
+        key: DESIGN_TOKEN_CONFIG_KEY,
+        value: JSON.stringify(JSON.parse(this.designTokenStore?.getAllStringTokens() || '')),
+      },
+      {
+        version: VERSION,
+        key: DESIGN_TOKEN_SCSS_KEY,
+        value: this.designTokenStore?.generateCssString() || '',
+      },
+    ]);
 
     toast.success('保存成功');
   };
@@ -110,11 +161,14 @@ class StyleGuideStore {
     const { domain, readable } = window.CONFIG.oss_config;
 
     const cssUrl = `${nanoid()}/style-guide/personalized_style.css`;
-    const { url } = await httpClient<{ url: string }>('/api/v1/fileserver/sign/upload', {
-      path: `${readable}/${cssUrl}`,
-      contentType: 'text/css',
-      contentLength: cssFile?.size,
-    });
+    const { url } = await httpClient<{ url: string }>(
+      '/api/v1/fileserver/sign/upload',
+      {
+        path: `${readable}/${cssUrl}`,
+        contentType: 'text/css',
+        contentLength: cssFile?.size,
+      },
+    );
 
     fetch(url, {
       method: 'put',
