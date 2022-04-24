ea9f2f7cd236a2c74bad58e59b1670e90e583118
Bunny <qimiao@yunify.com>
2022-04-24 13:39:16 +0800

    refactor: Temporary commit

--- clients/portal/props-spec.ts
+++ clients/portal/props-spec.ts
@@ -1,318 +1,341 @@
-export const props_spec = [{
-  key: 'button',
-  spec: [
-    {
-      label: '按钮名称',
-      name: 'children',
-      type: 'string',
-    },
-    {
-      label: '按钮类型',
-      name: 'modifier',
-      type: 'string',
-      will: 'select',
-      willProps: {
-        option: [
-          {
-            label: '默认',
-            value: '',
-          },
-          {
-            label: '主要',
-            value: 'primary',
-          },
-          {
-            label: '危险',
-            value: 'danger',
-          },
-        ],
+export type Props_Spec = {
+  label: string;
+  name: string;
+  type: 'string' | 'number' | 'boolean' | string;
+  initialValue?: any;
+  will?: string;
+  willProps?: Record<string, unknown>;
+  desc?: string;
+}
+
+export const props_spec: {key: string; spec: Props_Spec[]}[] = [
+  {
+    key: 'button',
+    spec: [
+      {
+        label: '按钮名称',
+        name: 'children',
+        type: 'string',
       },
-      initialValue: '',
-    },
-    {
-      label: '按钮功能',
-      name: 'type',
-      type: 'string',
-      will: 'select',
-      willProps: {
-        option: [
-          {
-            label: '按钮',
-            value: 'button',
-          },
-          {
-            label: '提交',
-            value: 'submit',
-          },
-          {
-            label: '重置',
-            value: 'reset',
-          },
-        ],
+      {
+        label: '按钮类型',
+        name: 'modifier',
+        type: 'string',
+        will: 'Switch',
+        willProps: {
+          option: [
+            {
+              label: '默认',
+              value: '',
+            },
+            {
+              label: '主要',
+              value: 'primary',
+            },
+            {
+              label: '危险',
+              value: 'danger',
+            },
+          ],
+        },
+        initialValue: '',
       },
-      initialValue: 'button',
-    },
-    {
-      label: '禁用',
-      name: 'forbidden',
-      type: 'boolean',
-      initialValue: false,
-    },
-    {
-      label: '加载状态',
-      name: 'loading',
-      type: 'boolean',
-    },
-    {
-      label: '加载文本',
-      name: 'loadingText',
-      type: 'string',
-    },
-    {
-      label: '图标名称',
-      name: 'iconName',
-      type: 'string',
-    },
-    {
-      label: '图标大小',
-      name: 'iconSize',
-      type: 'number',
-    },
-    {
-      label: '点击事件',
-      name: 'onClick',
-      type: 'function',
-    },
-  ],
-}, {
-  key: 'CheckboxGroup',
-  spec: [
-    {
-      label: '选项',
-      name: 'options',
-      type: 'object',
-      initialValue: [
-        {
-          label: '选项一',
-          value: 'option1',
+      {
+        label: '按钮功能',
+        name: 'type',
+        type: 'string',
+        will: 'select',
+        willProps: {
+          option: [
+            {
+              label: '按钮',
+              value: 'button',
+            },
+            {
+              label: '提交',
+              value: 'submit',
+            },
+            {
+              label: '重置',
+              value: 'reset',
+            },
+          ],
         },
-        {
-          label: '选项二',
-          value: 'option2',
-        },
-        {
-          label: '选项三',
-          value: 'option3',
-          disabled: true,
-        },
-      ],
-    },
-    {
-      label: '默认值',
-      name: 'value',
-      type: 'string',
-      initialValue: 'option1',
-    },
-    {
-      label: '禁用',
-      name: 'disabled',
-      type: 'boolean',
-    },
-    {
-      label: 'onChange',
-      name: 'onChange',
-      type: 'function',
-    },
-  ],
-}, {
-  key: 'Divider',
-  spec: [
-    {
-      label: '选项',
-      name: 'direction',
-      type: 'string',
-      will: 'select',
-      willProps: [
-        {
-          label: '水平',
-          value: 'horizontal',
-        },
-        {
-          label: '垂直',
-          value: 'vertical',
-        },
-      ],
-      initialValue: 'horizontal',
-    },
-    {
-      label: '大小',
-      name: 'size',
-      type: 'string',
-      initialValue: '100%',
-    },
-    {
-      label: '粗细',
-      name: 'thickness',
-      type: 'string',
-      initialValue: '1px',
-    },
-  ],
-}, {
-  key: 'Input',
-  spec: [
-    {
-      label: '占位符',
-      name: 'placeholder',
-      type: 'string',
-      initialValue: '请输入内容',
-    },
-    {
-      label: 'name',
-      name: 'name',
-      type: 'string',
-    },
-    {
-      label: '禁用',
-      name: 'disabled',
-      type: 'boolean',
-    },
-    {
-      label: '只读',
-      name: 'readOnly',
-      type: 'boolean',
-    },
-    {
-      label: '错误',
-      name: 'error',
-      type: 'boolean',
-    },
-    {
-      label: '最大长度',
-      name: 'maxLength',
-      type: 'number',
-    },
-    {
-      label: 'onChange',
-      name: 'onChange',
-      type: 'function',
-    },
-    {
-      label: 'onKeydown',
-      name: 'onKeydown',
-      type: 'function',
-    },
-    {
-      label: 'onFocus',
-      name: 'onFocus',
-      type: 'function',
-    },
-    {
-      label: 'onBlur',
-      name: 'onBlur',
-      type: 'function',
-    },
-  ],
-}, {
-  key: 'Breadcrumb',
-  spec: [
-    {
-      label: '',
-      name: 'segments',
-      type: 'object',
-      initialValue: [
-        {
-          key: 'one',
-          text: '一级菜单',
-        },
-        {
-          key: 'two',
-          text: '二级菜单',
-          path: 'https://www.google.com/',
-        },
-      ],
-    },
-    {
-      name: 'separator',
-      type: 'string',
-      initialValue: '/',
-    },
-  ],
-}, {
-  key: 'Loading',
-  spec: [
-    {
-      label: '描述',
-      name: 'desc',
-      type: 'string',
-    },
-    {
-      label: '图标大小',
-      name: 'iconSize',
-      type: 'number',
-    },
-    {
-      label: '水平',
-      name: 'vertical',
-      type: 'boolean',
-    },
-  ],
-}, {
-  key: 'RadioGroup',
-  spec: [
-    {
-      label: '选项',
-      name: 'options',
-      type: 'object',
-      initialValue: [
-        {
-          label: '选项一',
-          value: 'option1',
-        },
-        {
-          label: '选项二',
-          value: 'option2',
-        },
-        {
-          label: '选项三',
-          value: 'option3',
-          disabled: true,
-        },
-      ],
-    },
-    {
-      label: '默认值',
-      name: 'value',
-      type: 'string',
-      initialValue: 'option1',
-    },
-    {
-      label: '禁用',
-      name: 'disabled',
-      type: 'boolean',
-    },
-    {
-      label: 'onChange',
-      name: 'onChange',
-      type: 'function',
-    },
-  ],
-}, {
-  key: 'Switch',
-  spec: [
-    {
-      label: '开启文案',
-      name: 'onText',
-      type: 'string',
-    },
-    {
-      label: '关闭文案',
-      name: 'offText',
-      type: 'string',
-    },
-    {
-      label: 'onChange',
-      name: 'onChange',
-      type: 'function',
-    },
-  ],
-}];
+        initialValue: 'button',
+      },
+      {
+        label: '禁用',
+        name: 'forbidden',
+        type: 'boolean',
+        initialValue: false,
+      },
+      {
+        label: '加载状态',
+        name: 'loading',
+        type: 'boolean',
+      },
+      {
+        label: '加载文本',
+        name: 'loadingText',
+        type: 'string',
+      },
+      {
+        label: '图标名称',
+        name: 'iconName',
+        type: 'string',
+      },
+      {
+        label: '图标大小',
+        name: 'iconSize',
+        type: 'number',
+      },
+      {
+        label: '点击事件',
+        name: 'onClick',
+        type: 'function',
+      },
+    ],
+  },
+  // {
+  //   key: 'CheckboxGroup',
+  //   spec: [
+  //     {
+  //       label: '选项',
+  //       name: 'options',
+  //       type: 'object',
+  //       initialValue: [
+  //         {
+  //           label: '选项一',
+  //           value: 'option1',
+  //         },
+  //         {
+  //           label: '选项二',
+  //           value: 'option2',
+  //         },
+  //         {
+  //           label: '选项三',
+  //           value: 'option3',
+  //           disabled: true,
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       label: '默认值',
+  //       name: 'value',
+  //       type: 'string',
+  //       initialValue: 'option1',
+  //     },
+  //     {
+  //       label: '禁用',
+  //       name: 'disabled',
+  //       type: 'boolean',
+  //     },
+  //     {
+  //       label: 'onChange',
+  //       name: 'onChange',
+  //       type: 'function',
+  //     },
+  //   ],
+  // },
+  // {
+  //   key: 'Divider',
+  //   spec: [
+  //     {
+  //       label: '选项',
+  //       name: 'direction',
+  //       type: 'string',
+  //       will: 'select',
+  //       willProps: {
+  //         options: [
+  //           {
+  //             label: '水平',
+  //             value: 'horizontal',
+  //           },
+  //           {
+  //             label: '垂直',
+  //             value: 'vertical',
+  //           },
+  //         ],
+  //       },
+  //       initialValue: 'horizontal',
+  //     },
+  //     {
+  //       label: '大小',
+  //       name: 'size',
+  //       type: 'string',
+  //       initialValue: '100%',
+  //     },
+  //     {
+  //       label: '粗细',
+  //       name: 'thickness',
+  //       type: 'string',
+  //       initialValue: '1px',
+  //     },
+  //   ],
+  // },
+  // {
+  //   key: 'Input',
+  //   spec: [
+  //     {
+  //       label: '占位符',
+  //       name: 'placeholder',
+  //       type: 'string',
+  //       initialValue: '请输入内容',
+  //     },
+  //     {
+  //       label: 'name',
+  //       name: 'name',
+  //       type: 'string',
+  //     },
+  //     {
+  //       label: '禁用',
+  //       name: 'disabled',
+  //       type: 'boolean',
+  //     },
+  //     {
+  //       label: '只读',
+  //       name: 'readOnly',
+  //       type: 'boolean',
+  //     },
+  //     {
+  //       label: '错误',
+  //       name: 'error',
+  //       type: 'boolean',
+  //     },
+  //     {
+  //       label: '最大长度',
+  //       name: 'maxLength',
+  //       type: 'number',
+  //     },
+  //     {
+  //       label: 'onChange',
+  //       name: 'onChange',
+  //       type: 'function',
+  //     },
+  //     {
+  //       label: 'onKeydown',
+  //       name: 'onKeydown',
+  //       type: 'function',
+  //     },
+  //     {
+  //       label: 'onFocus',
+  //       name: 'onFocus',
+  //       type: 'function',
+  //     },
+  //     {
+  //       label: 'onBlur',
+  //       name: 'onBlur',
+  //       type: 'function',
+  //     },
+  //   ],
+  // },
+  // {
+  //   key: 'Breadcrumb',
+  //   spec: [
+  //     {
+  //       label: '',
+  //       name: 'segments',
+  //       type: 'object',
+  //       initialValue: [
+  //         {
+  //           key: 'one',
+  //           text: '一级菜单',
+  //         },
+  //         {
+  //           key: 'two',
+  //           text: '二级菜单',
+  //           path: 'https://www.google.com/',
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       label: '',
+  //       name: 'separator',
+  //       type: 'string',
+  //       initialValue: '/',
+  //     },
+  //   ],
+  // },
+  // {
+  //   key: 'Loading',
+  //   spec: [
+  //     {
+  //       label: '描述',
+  //       name: 'desc',
+  //       type: 'string',
+  //     },
+  //     {
+  //       label: '图标大小',
+  //       name: 'iconSize',
+  //       type: 'number',
+  //     },
+  //     {
+  //       label: '水平',
+  //       name: 'vertical',
+  //       type: 'boolean',
+  //     },
+  //   ],
+  // },
+  // {
+  //   key: 'RadioGroup',
+  //   spec: [
+  //     {
+  //       label: '选项',
+  //       name: 'options',
+  //       type: 'object',
+  //       initialValue: [
+  //         {
+  //           label: '选项一',
+  //           value: 'option1',
+  //         },
+  //         {
+  //           label: '选项二',
+  //           value: 'option2',
+  //         },
+  //         {
+  //           label: '选项三',
+  //           value: 'option3',
+  //           disabled: true,
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       label: '默认值',
+  //       name: 'value',
+  //       type: 'string',
+  //       initialValue: 'option1',
+  //     },
+  //     {
+  //       label: '禁用',
+  //       name: 'disabled',
+  //       type: 'boolean',
+  //     },
+  //     {
+  //       label: 'onChange',
+  //       name: 'onChange',
+  //       type: 'function',
+  //     },
+  //   ],
+  // },
+  // {
+  //   key: 'Switch',
+  //   spec: [
+  //     {
+  //       label: '开启文案',
+  //       name: 'onText',
+  //       type: 'string',
+  //     },
+  //     {
+  //       label: '关闭文案',
+  //       name: 'offText',
+  //       type: 'string',
+  //       desc: '按钮的类型决定按钮的样式状态',
+  //     },
+  //     {
+  //       label: 'onChange',
+  //       name: 'onChange',
+  //       type: 'function',
+  //     },
+  //   ],
+  // },
+];
--- clients/portal/test.tsx
+++ clients/portal/test.tsx
@@ -1,30 +1,13 @@
-import { Artery, Node } from '@one-for-all/artery';
-import { ArteryRenderer } from '@one-for-all/artery-renderer';
 import React, { useEffect, useState } from 'react';
+import { Artery, HTMLNode, Node } from '@one-for-all/artery';
+import { ArteryRenderer } from '@one-for-all/artery-renderer';
+
+import Toggle from '@c/toggle';
 import { genNodeID } from './modules/apps-management/pages/app-details/view-orchestration/helpers/utils';
 
-import { props_spec } from './props-spec';
+import { Props_Spec, props_spec } from './props-spec';
 
-const reactNode = {
-  id: genNodeID(),
-  type: 'react-component',
-  packageName: 'ofa-ui',
-  packageVersion: 'latest',
-  exportName: 'page',
-  label: 'react-node',
-  props: {
-    style: {
-      type: 'constant_property',
-      value: {
-        width: '100%',
-        height: '100%',
-      },
-    },
-  },
-  children: [],
-};
-
-const initConfigWrapperNode: Node = {
+const configWrapperNode: HTMLNode = {
   id: genNodeID(),
   type: 'html-element',
   name: 'div',
@@ -38,23 +21,64 @@ const initConfigWrapperNode: Node = {
       },
     },
   },
-  // children: [{
-  //   id: genNodeID(),
-  //   type: 'html-element',
-  //   name: 'div',
-  //   props: {
-  //     children: {
-  //       type: 'constant_property',
-  //       value: '置表单',
-  //     },
-  //   },
-  // }],
 };
 
 const initConfigArtery: Artery = {
-  node: initConfigWrapperNode,
+  node: configWrapperNode,
   apiStateSpec: {},
   sharedStatesSpec: {},
+};
+
+const WILL_COMPONENT_MAP: Record<string, string> = {
+  string: 'Input',
+  number: 'NumberPicker',
+  boolean: 'Toggle',
+};
+
+function buildTextItem(item: Props_Spec, style?: Record<string, string>): Node {
+  return {
+    id: genNodeID(),
+    type: 'html-element',
+    name: 'div',
+    label: item.label,
+    props: {
+      children: {
+        type: 'constant_property',
+        value: item.label,
+      },
+      style: {
+        type: 'constant_property',
+        value: style,
+      },
+    },
+  };
+}
+
+function buildFieldItem(item: Props_Spec): Node {
+  return {
+    id: genNodeID(),
+    type: 'react-component',
+    packageName: 'node-carve',
+    packageVersion: '1.0.0',
+    exportName: item.will || WILL_COMPONENT_MAP[item.type],
+    label: item.label,
+    props: {
+      path: {
+        type: 'constant_property',
+        value: `${item.name}`,
+      },
+      initValue: {
+        type: 'constant_property',
+        value: `${item.initialValue}`,
+      },
+    },
+  };
+}
+
+const repository = {
+  'node-carve@1.0.0': {
+    Toggle,
+  },
 };
 
 function Test(): JSX.Element {
@@ -62,49 +86,37 @@ function Test(): JSX.Element {
 
   useEffect(() => {
     const configSpec = props_spec.pop()?.spec;
-    const configNodes = configSpec?.map((item) => {
-      const labelNode: Node = {
+    const configNodes = configSpec?.map((item: Props_Spec) => {
+      const wrapperNode: HTMLNode = {
         id: genNodeID(),
         type: 'html-element',
         name: 'div',
         label: item.label,
-        props: {
-          children: {
-            type: 'constant_property',
-            value: item.label,
-          },
-        },
       };
-      const itemNode: Node = {
-        id: genNodeID(),
-        type: 'html-element',
-        name: 'div',
-        label: item.label,
-        props: {
-          children: {
-            type: 'constant_property',
-            value: item.label,
-          },
-        },
-      };
+      const labelNode: Node = buildTextItem(item, { padding: '8px 0' });
+      const itemNode: Node = buildFieldItem(item);
+      const nodes: Node[] = [labelNode, itemNode];
 
-      return labelNode;
+      if (item.desc) {
+        const descNode: Node = buildTextItem(item, { paddingTop: '8px', color: '#94A3B8', fontSize: '12px' });
+
+        return {
+          ...wrapperNode,
+          children: [
+            ...nodes,
+            descNode,
+          ],
+        };
+      }
+
+      return {
+        ...wrapperNode,
+        children: nodes,
+      };
     });
 
-    const node: Node = {
-      id: genNodeID(),
-      type: 'html-element',
-      name: 'div',
-      label: '属性配置',
-      props: {
-        style: {
-          type: 'constant_property',
-          value: {
-            width: '100%',
-            height: '100vh',
-          },
-        },
-      },
+    const node: HTMLNode = {
+      ...configWrapperNode,
       children: configNodes,
     };
 
@@ -115,10 +127,14 @@ function Test(): JSX.Element {
     });
   }, []);
 
+  useEffect(() => {
+    console.log(artery);
+  }, [artery]);
+
   return (
     <ArteryRenderer
       artery={artery}
-      // plugins={{ apiSpecAdapter: adapter, repository, componentLoader, refLoader }}
+      plugins={{ repository }}
     />
   );
 }
