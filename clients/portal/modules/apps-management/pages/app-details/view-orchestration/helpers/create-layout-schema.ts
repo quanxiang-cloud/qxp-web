fec1d16e99c40948ffec15aea937a22ab716c644
Bunny miao <49241072+hhhqm@users.noreply.github.com>
2022-04-26 11:33:35 +0800

    fix: Fix the root wrapper node height (#95)
    Co-authored-by: Bunny <qimiao@yunify.com>

--- clients/portal/modules/apps-management/pages/app-details/view-orchestration/helpers/create-layout-schema.ts
+++ clients/portal/modules/apps-management/pages/app-details/view-orchestration/helpers/create-layout-schema.ts
@@ -1,4 +1,4 @@
-import { Node, RouteNode } from '@one-for-all/artery';
+import { Node, NodeProperties, RouteNode } from '@one-for-all/artery';
 
 import {
   LAYOUT_CHILD_TYPE_FRAGMENT_CONTAINER,
@@ -16,35 +16,44 @@ export type LayoutSchemaProps = {
   isRoot?: boolean,
 }
 
+const ROOT_NODE_STYLE: NodeProperties = {
+  style: {
+    type: 'constant_property',
+    value: { height: '100vh', overflow: 'auto' },
+  },
+};
+
 export default function createLayoutSchema({
   layoutInfo,
   refSchemaKey,
   initialChild,
   isRoot,
 }: LayoutSchemaProps ): Node {
+  const layoutProperties: NodeProperties = {
+    'data-internal-node': {
+      type: 'constant_property',
+      value: true,
+    },
+    'data-layout': {
+      type: 'constant_property',
+      value: true,
+    },
+    'data-layout-type': {
+      type: 'constant_property',
+      value: layoutInfo.type,
+    },
+    'data-layout-description': {
+      type: 'constant_property',
+      value: layoutInfo.description || '',
+    },
+  };
+
   return {
     id: isRoot ? ROOT_NODE_ID : genNodeID(),
     label: layoutInfo.name,
     type: 'html-element',
     name: 'div',
-    props: {
-      'data-internal-node': {
-        type: 'constant_property',
-        value: true,
-      },
-      'data-layout': {
-        type: 'constant_property',
-        value: true,
-      },
-      'data-layout-type': {
-        type: 'constant_property',
-        value: layoutInfo.type,
-      },
-      'data-layout-description': {
-        type: 'constant_property',
-        value: layoutInfo.description || '',
-      },
-    },
+    props: isRoot ? Object.assign(layoutProperties, ROOT_NODE_STYLE) : layoutProperties,
     children: [
       {
         id: genNodeID(),
