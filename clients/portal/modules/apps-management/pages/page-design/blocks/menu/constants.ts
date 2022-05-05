ae0b9c6c20691837a411a3a08fd34661f774319f
nfwyst <nfwyst@gmail.com>
2022-05-05 18:45:52 +0800

    feat: Support shared props spec (#109)

--- clients/portal/modules/apps-management/pages/page-design/blocks/menu/constants.ts
+++ clients/portal/modules/apps-management/pages/page-design/blocks/menu/constants.ts
@@ -4,23 +4,23 @@ export const defaultPackages: Package[] = [
   {
     name: 'all',
     label: '所有组件',
-    version: 'latest',
+    version: '1.0.0',
   },
   {
     name: 'system-components',
     label: '系统组件',
-    version: 'latest',
+    version: '1.0.0',
   },
   {
     name: '@one-for-all/ui',
     label: 'UI (不推荐)',
-    version: 'latest',
+    version: '0.3.3',
     categories: ['布局组件', '基础组件', '表单组件'],
   },
   {
     name: 'ofa-ui',
     label: 'legacy',
-    version: 'latest',
+    version: '1.0.0',
     categories: ['布局组件', '基础组件', '表单组件', '高级组件'],
     hide: true,
   },
