0ee6806d98d399c6dcf0ef5040ab886545679041
Mark <markduan@yunify.com>
2022-05-13 15:49:39 +0800

    refactor: Copy host window global variable to simulator iframe

--- clients/portal/modules/apps-management/pages/page-design/blocks/simulator/index.tsx
+++ clients/portal/modules/apps-management/pages/page-design/blocks/simulator/index.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import ArterySimulator from '@one-for-all/artery-simulator';
+import React, { useEffect, useRef, useState } from 'react';
+import ArterySimulator, { SimulatorRef } from '@one-for-all/artery-simulator';
 import { BlockItemProps } from '@one-for-all/artery-engine';
 
 import { BlocksCommunicationType } from '../../types';
@@ -11,9 +11,18 @@ import pluginsSrc from 'dll:./../../../../../../../../tmp/TEMPORARY_PATCH_FOR_AR
 function SimulatorBlock(props: BlockItemProps<BlocksCommunicationType>): JSX.Element {
   const { artery, onChange, activeNode, setActiveNode } = props;
   const [activeModalLayer, setActiveModalLayer] = useState<string | undefined>();
+  const simulatorRef = useRef<SimulatorRef>(null);
+
+  useEffect(() => {
+    if (simulatorRef.current?.iframe?.contentWindow) {
+      simulatorRef.current.iframe.contentWindow.CONFIG = window.CONFIG;
+      simulatorRef.current.iframe.contentWindow.USER = window.USER;
+    }
+  }, []);
 
   return (
     <ArterySimulator
+      ref={simulatorRef}
       className="artery-simulator"
       artery={artery}
       pluginsSrc={pluginsSrc}
