027d4ca4480eb43bb61c126fa93ec06e35da8979
Mark <markduan@yunify.com>
2022-05-16 11:08:48 +0800

    refactor: Move system-componet repo to common components

--- clients/components/artery-renderer/repository.ts
+++ clients/components/artery-renderer/repository.ts
@@ -1,18 +1,12 @@
-import { Repository } from '@one-for-all/artery-renderer';
+import type { Repository } from '@one-for-all/artery-renderer';
 
 import SimpleViewRenders from '@c/simple-view-render';
-import TaskList from '@c/task-lists';
-import UserAvatarMenu from '@c/user-avatar-menu';
-
-const systemComponents = {
-  SystemTaskList: TaskList,
-  UserMenuAvatar: UserAvatarMenu,
-};
+import repoSystemComponents from '@c/repo-system-components';
 
 // todo fix this
 const repository: Repository = {
-  'SimpleViewRenders@1.0.0': SimpleViewRenders,
-  'system-components@1.0.0': systemComponents,
+  ...SimpleViewRenders,
+  ...repoSystemComponents,
 };
 
 export default repository;
--- clients/components/simple-view-render/index.tsx
+++ clients/components/simple-view-render/index.tsx
@@ -1,5 +1,11 @@
+import type { Repository } from '@one-for-all/artery-renderer';
+
 import ExternalViewRender from './external-view-render';
 import StaticViewRender from './static-view-render';
 import TableViewDetail from './table-schema-view-render';
 
-export default { ExternalViewRender, StaticViewRender, TableSchemaViewRender: TableViewDetail };
+const repoSimplyViewRender: Repository = {
+  'SimpleViewRenders@1.0.0': { ExternalViewRender, StaticViewRender, TableSchemaViewRender: TableViewDetail },
+};
+
+export default repoSimplyViewRender;
