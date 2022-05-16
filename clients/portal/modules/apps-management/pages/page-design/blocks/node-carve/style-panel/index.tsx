f67d35c9ec8e42863a62d94a8b6ebaeefe0c0fde
EricReevess <39663838+EricReevess@users.noreply.github.com>
2022-05-16 18:15:12 +0800

    feat: Add monaco editor for custom page engine compontent style (#146)
    Co-authored-by: Reeves <reevesli@yunify.com>

--- clients/portal/modules/apps-management/pages/page-design/blocks/node-carve/style-panel/index.tsx
+++ clients/portal/modules/apps-management/pages/page-design/blocks/node-carve/style-panel/index.tsx
@@ -1,10 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import cs from 'classnames';
-import Editor from '@uiw/react-codemirror';
-import { javascript } from '@codemirror/lang-javascript';
-
-import { Modal, Icon, Button, toast } from '@one-for-all/ui';
 
 import LayoutConfig from './layout-config';
 import PositionConfig from './position-config';
@@ -27,30 +23,19 @@ interface Props {
 function StylePanel({ className }: Props): JSX.Element {
   const { register, getValues, setValue, reset } = useForm();
   const [values, setValues] = useState<any>({});
-  const [modalOpen, setModalOpen] = useState(false);
-  const [editorVal, setEditorVal] = useState<any>({});
 
-  const { artery, activeNode, onArteryChange } = useConfigContext() ?? {};
+  const { artery, rawActiveNode, onArteryChange } = useConfigContext() ?? {};
 
   useEffect(() => {
-    if (activeNode) {
+    if (rawActiveNode) {
       setValues(getCurStyle());
       reset(getCurStyle());
     }
-  }, [activeNode?.id]);
-
-  function getEditorValues(values: any): string {
-    let _values = values;
-    if (!checkStyles(_values)) {
-      _values = {};
-    }
-    return JSON.stringify(values, null, 2);
-  }
+  }, [rawActiveNode?.id]);
 
   function getCurStyle(): React.CSSProperties {
-    if (activeNode) {
-      const defaultStyles = get(activeNode, 'props.style.value', {});
-      setEditorVal(getEditorValues(defaultStyles));
+    if (rawActiveNode) {
+      const defaultStyles = get(rawActiveNode, 'props.style.value', {});
       const newStyles = parseStyleToForm(defaultStyles);
       return newStyles;
     }
@@ -58,40 +43,7 @@ function StylePanel({ className }: Props): JSX.Element {
     return {};
   }
 
-  function checkStyles(vals: any): boolean {
-    try {
-      if (typeof vals === 'string') {
-        JSON.parse(vals);
-      }
-      return true;
-    } catch (err: any) {
-      toast.error(`错误的 CSS值，请填写 JSX 格式的 JSON object: ${err.message}`);
-      return false;
-    }
-  }
-
-  function saveStyle(): void {
-    // save editor values
-    if (checkStyles(editorVal)) {
-      setModalOpen(false);
-      // setValues(JSON.parse(editorVal));
-      const newValues = JSON.parse(editorVal);
-      const _newValues = parseStyleToForm(newValues);
-      setValues(_newValues);
-      reset(_newValues);
-      if (activeNode && artery) {
-        const newArtery = updateNodeProperty(
-          activeNode,
-          'props.style',
-          { type: 'constant_property', value: newValues },
-          artery,
-        );
-        onArteryChange?.(newArtery);
-      }
-    }
-  }
-
-  if (!activeNode) {
+  if (!rawActiveNode) {
     return (
       <div className='flex justify-center items-center flex-col h-full'>
         <p>当前层级没有内容</p>
@@ -104,25 +56,19 @@ function StylePanel({ className }: Props): JSX.Element {
     const _values = getValues();
     setValues(_values);
     const newValues = formatStyles(_values);
-    if (activeNode && artery) {
+    if (rawActiveNode && artery) {
       const newArtery = updateNodeProperty(
-        activeNode,
+        rawActiveNode,
         'props.style',
         { type: 'constant_property', value: newValues },
         artery,
       );
       onArteryChange?.(newArtery);
     }
-    setEditorVal(getEditorValues(newValues));
   }
 
   return (
     <div className={cs(styles.stylePanel, className)}>
-      <div className='mt-8 ml-8 mb-8'>
-        <Button className='flex items-center' onClick={() => setModalOpen(true)}>
-          <Icon name='code' />源码编辑
-        </Button>
-      </div>
       <form onChange={handleFormChange}>
         <Section title='画布' defaultExpand>
           <LayoutConfig initValues={values} register={register} setValue={setValue} />
@@ -166,36 +112,6 @@ function StylePanel({ className }: Props): JSX.Element {
           />
         </Section>
       </form>
-
-      {modalOpen && (
-        <Modal
-          title='编辑样式'
-          onClose={() => setModalOpen(false)}
-          footerBtns={[
-            {
-              key: 'close',
-              iconName: 'close',
-              onClick: () => setModalOpen(false),
-              text: '取消',
-            },
-            {
-              key: 'check',
-              iconName: 'check',
-              modifier: 'primary',
-              onClick: saveStyle,
-              text: '确认',
-            },
-          ]}
-        >
-          <Editor
-            value={editorVal}
-            height="480px"
-            theme='light'
-            extensions={[javascript()]}
-            onChange={(val) => setEditorVal(val)}
-          />
-        </Modal>
-      )}
     </div>
   );
 }
