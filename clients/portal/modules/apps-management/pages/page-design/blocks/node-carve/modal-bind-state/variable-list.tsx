62aa0a670782b567af9c3284562c6a7e97055610
Bunny miao <49241072+hhhqm@users.noreply.github.com>
2022-05-16 14:12:09 +0800

    refactor: Refactor bind variables (#142)
    * refactor: Refactor the bind variables way in page design
    
    * feat: Add handler of bind
    
    * fix: Fix the value reset when change tab id
    
    * refactor: Refactor the code of modal-bind-state
    
    * refactor: Refactor the parseExpression
    
    * refactor: Use regex to macth variables
    
    * fix: Add debugger for code editor
    
    * fix: Fix
    
    * refactor: Refactor the code edior in tab
    
    * refactor: Refactor the col of bind variables and operators
    
    * refactor: Refactor the variableList
    
    * refactor: Refactor the code of body in convertor
    
    * refactor: Add css class for modal-bind-state
    
    * feat: Add unbind btn
    
    * fix: Add dependency of code editor initialize value
    
    * refactor: Remove useless console log
    
    * refactor: Remove mock data of artery data source
    
    * refactor: Refactor the style of bind-item class
    
    * refactor: Refactor the ui of shouldRender condition interface
    
    * refactor: Refactor the VariableBindConf modal
    
    * refactor: Refactor the variable list
    
    * fix: Fix the bug of init convertor
    
    * refactor: Merge develop
    
    Co-authored-by: Bunny <qimiao@yunify.com>

---
+++ clients/portal/modules/apps-management/pages/page-design/blocks/node-carve/modal-bind-state/variable-list.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { ComputedDependency } from '@one-for-all/artery';
+
+import Section from '../../../utils/section';
+import { EditorRefType } from './code-editor';
+import dataSource from '../../../stores/data-source';
+
+import styles from './index.m.scss';
+
+type Props = {
+  boundVariables: ComputedDependency[];
+  updateBoundVariables: (newBoundVariables: ComputedDependency[]) => void;
+  editorRef?: React.MutableRefObject<EditorRefType | undefined>;
+}
+
+function VariableList({ boundVariables, updateBoundVariables, editorRef }: Props): JSX.Element {
+  const variableNames = boundVariables?.map(({ depID }) => depID);
+  const sharedStates = Object.entries(dataSource.sharedState).map(([_, value]) => JSON.parse(value));
+  const apiStates = Object.entries(dataSource.apiState).map(([_, value]) => JSON.parse(value));
+
+  function handleVariableClick(variable: string): void {
+    editorRef?.current && editorRef.current.onInsertText(variable);
+
+    if (variableNames.includes(variable)) return;
+    updateBoundVariables([...boundVariables, { depID: variable, type: 'shared_state' }]);
+  }
+
+  return (
+    <>
+      <Section title="自定义变量" defaultExpand>
+        {!sharedStates.length && <div className="text-12 text-gray-400">暂无自定义变量，请在数据源中添加</div>}
+        {!!sharedStates.length && sharedStates.map(({ name }) => {
+          return (
+            <div
+              key={name}
+              className={styles['list-item']}
+              onClick={() => handleVariableClick(name)}
+            >
+              {name}
+            </div>
+          );
+        })}
+      </Section>
+      <Section title="API变量" defaultExpand>
+        {!apiStates.length && <div className="text-12 text-gray-400">暂无 API 变量，请在数据源中添加</div>}
+        {!!apiStates.length && (
+          <>
+            {apiStates.map(({ name }) => {
+              return (
+                <div
+                  key={name}
+                  className={styles['list-item']}
+                  onClick={() => console.log('bind: ', name, 'api_state')}
+                >
+                  {name}
+                </div>
+              );
+            })}
+          </>
+        )}
+      </Section>
+    </>
+  );
+}
+
+export default VariableList;
