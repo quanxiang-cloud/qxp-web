b32dfc709f26ed5b88ba1cdcd40c647b72c182de
songwei <songwei@yunify.com>
2022-05-18 15:52:28 +0800

    fix: Add config of uploaded file

--- clients/portal/modules/apps-management/pages/page-design/registry/system-components/index.ts
+++ clients/portal/modules/apps-management/pages/page-design/registry/system-components/index.ts
@@ -1,5 +1,6 @@
 import SystemTaskList from '@c/task-lists';
 import UserMenuAvatar from '@c/user-avatar-menu';
+import FileUpload from './file-upload/file-upload';
 import GridContainer from './gridContainer';
 import a from './a';
 import div from './div';
@@ -9,6 +10,7 @@ import span from './span';
 export default {
   UserMenuAvatar,
   SystemTaskList,
+  FileUpload,
   GridContainer,
   a,
   div,
