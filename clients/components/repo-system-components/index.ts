02fd13a9adf1ee2fdd393d31deb9059f70e0e73e
songwei <songwei@yunify.com>
2022-05-18 16:12:15 +0800

    fix: Add file upload into repo system

--- clients/components/repo-system-components/index.ts
+++ clients/components/repo-system-components/index.ts
@@ -3,6 +3,7 @@ import type { Repository } from '@one-for-all/artery-renderer';
 import versionMap from '@pageDesign/blocks/fountainhead/config/name-version-map';
 import SystemTaskList from '@c/task-lists';
 import UserMenuAvatar from '@c/user-avatar-menu';
+import FileUpload from './file-upload';
 
 import GridContainer from './grid-container';
 import a from './a';
@@ -14,6 +15,7 @@ const repoSystemComponents: Repository = {
   [`system-components@${versionMap['system-components']}`]: {
     UserMenuAvatar,
     SystemTaskList,
+    FileUpload,
     GridContainer,
     a,
     div,
