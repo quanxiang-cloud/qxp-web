13731bf485d73a25a80056581a7fab7e46f13a29
iriskang <iriskang@yunify.com>
2022-06-23 17:31:37 +0800

    fix: Fix classnames

--- clients/portal/modules/apps-management/pages/app-details/roles-and-permissions/api-auth/auth-details/data-range.tsx
+++ clients/portal/modules/apps-management/pages/app-details/roles-and-permissions/api-auth/auth-details/data-range.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { observer } from 'mobx-react';
-import cs from 'classNames';
+import cs from 'classnames';
 
 import RadioGroup from '@c/radio/group';
 import Radio from '@c/radio';
