aaebffa473395937093aa83215bcf32f146217d3
iriskang <iriskang@yunify.com>
2022-12-27 10:58:59 +0800

    feat: Add project group table

---
+++ clients/portal/modules/apps-management/project-group/edit-project-modal.tsx
@@ -0,0 +1,118 @@
+import React from 'react';
+import { Input, Form } from 'antd';
+
+import Modal from '@c/modal';
+import toast from '@lib/toast';
+
+import store from './store';
+import { validateTemplateName } from './api';
+import { DISABLE_SPECIAL_SYMBOL_REG } from '../pages/entry/app-list/app-edit/created-edit-app';
+// import AppIconPicker from '../../app-list/app-edit/app-icon-picker';
+
+type Props = {
+  modalType: string;
+  templateInfo: TemplateInfo;
+  onCancel: () => void;
+}
+
+function EditProjectModal({ modalType, templateInfo, onCancel }: Props): JSX.Element {
+  const [form] = Form.useForm();
+  const isEdit = modalType === 'editTemplate';
+  const { addTemplate, editTemplate } = store;
+
+  async function handleSubmit(): Promise<void> {
+    const { name, icon } = form.getFieldsValue();
+
+    if (isEdit) {
+      await editTemplate(templateInfo.id, name, icon);
+      return onCancel();
+    }
+
+    validateTemplateName(name).then(() => {
+      return addTemplate({ ...templateInfo, name, appIcon: icon });
+    }).then(() => {
+      onCancel();
+    }).catch(() => toast.error('模版名称校验失败')).finally(store.fetchList);
+  }
+
+  return (
+    <Modal
+      title={isEdit ? '修改模版信息' : '确定保存为模版'}
+      onClose={onCancel}
+      className="static-modal text-12"
+      footerBtns={[
+        {
+          key: 'close',
+          iconName: 'close',
+          onClick: onCancel,
+          text: '取消',
+        },
+        {
+          key: 'check',
+          iconName: 'check',
+          modifier: 'primary',
+          onClick: handleSubmit,
+          text: isEdit ? '确定修改' : '确定保存',
+        },
+      ]}
+    >
+      <div className="px-20 py-16 text-12">
+        <Form
+          layout="vertical"
+          form={form}
+          initialValues={{
+            name: templateInfo.name,
+            icon: templateInfo.appIcon,
+          }}
+        >
+          <Form.Item
+            name="name"
+            label='项目组'
+            rules={[
+              {
+                required: true,
+                message: '请输入应用名称',
+              },
+              {
+                pattern: /^((?!(\ud83c[\udf00-\udfff])|(\ud83d[\udc00-\ude4f])|(\ud83d[\ude80-\udeff])).)*$/,
+                message: '不能输入emoji表情符号',
+              },
+              {
+                type: 'string',
+                max: 30,
+                message: '不能超过 30 个字符',
+              },
+              {
+                validator: (rule, value) => {
+                  if (!value) {
+                    return Promise.resolve();
+                  } else if (DISABLE_SPECIAL_SYMBOL_REG.test(value)) {
+                    return Promise.reject(new Error('不能包含特殊字符'));
+                  } else {
+                    return Promise.resolve();
+                  }
+                },
+              },
+            ]}
+          >
+            <Input placeholder="请输入项目名称" />
+          </Form.Item>
+          <Form.Item
+            name="icon"
+            label="模版图标:"
+            rules={[
+              {
+                required: true,
+                message: '请选择应用图标',
+              },
+            ]}
+          >
+            {/* <AppIconPicker /> */}
+          </Form.Item>
+        </Form>
+      </div>
+    </Modal>
+  );
+}
+
+export default EditProjectModal;
---
+++ clients/portal/modules/apps-management/project-group/index.tsx
@@ -0,0 +1,132 @@
+import React, { useEffect, useState } from 'react';
+import { observer } from 'mobx-react';
+import { UnionColumn } from 'react-table';
+
+import { toast } from '@one-for-all/ui';
+import Table from '@c/table';
+import Button from '@c/button';
+import TextHeader from '@c/text-header';
+import EmptyTips from '@c/empty-tips';
+
+import store from './store';
+import EditProjectModal from './edit-project-modal';
+// import DelTemplateModal from './template-edit/del-template-modal';
+// import CreatedAppModal from '../app-list/app-edit/created-app-modal';
+import { APPLICATION_CREATE } from '@portal/constants';
+
+function ProjectGroup(): JSX.Element {
+  const [modalType, setModalType] = useState('');
+  const { templateList, templateListLoading, fetchList, curTemplate, setCurTemplate } = store;
+
+  const COLUMNS: UnionColumn<TemplateInfo>[] = [
+    {
+      Header: '项目组',
+      accessor: 'name',
+    },
+    {
+      Header: '项目成员',
+      accessor: 'version',
+    },
+    {
+      Header: '备注',
+      accessor: 'appName',
+    },
+    {
+      Header: '操作',
+      accessor: (row) => {
+        return (
+          window.ADMIN_USER_FUNC_TAGS.includes(APPLICATION_CREATE) ?
+            (<>
+              <span
+                className="text-blue-600 cursor-pointer mr-20"
+                onClick={() => handelMenuClick('modify', row)}
+              >
+                编辑
+              </span>
+              <span
+                className="text-blue-600 cursor-pointer mr-20"
+                onClick={() => handelMenuClick('delete', row)}
+              >
+                删除
+              </span>
+            </>) :
+            '-'
+        );
+      },
+    },
+  ];
+
+  useEffect(() => {
+    fetchList().catch(() => {
+      toast.error('获取模版列表失败');
+    });
+    return () => {
+      setCurTemplate(store.templateList?.[0]);
+    };
+  }, []);
+
+  function handelMenuClick(_modalType: string, rowData: TemplateInfo): void {
+    setModalType(_modalType);
+    setCurTemplate(rowData);
+  }
+
+  function RenderModal() {
+    if (!curTemplate) {
+      return null;
+    }
+
+    return (
+      <>
+        {modalType === 'create' && (
+          <EditProjectModal
+            modalType={modalType}
+            templateInfo={curTemplate}
+            onCancel={() => setModalType('')}
+          />
+        )}
+        {/* {modalType === 'delTemplate' &&
+          <DelTemplateModal templateInfo={curTemplate} onCancel={() => setModalType('')} />
+        } */}
+
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div className="flex flex-col h-full">
+        <TextHeader
+          title="模版库"
+          desc=""
+          // action="👋 快速开始"
+          className="app-list-headertitle bg-gray-1000 px-20 py-16 header-background-image h-44"
+          itemTitleClassName="text-h6"
+        />
+        <div className="p-16 font-semibold flex justify-between items-center">
+          我的项目 · {templateList.length}
+          <div>
+            <Button
+              iconName="add"
+              onClick={() => setModalType('create')}
+              className="mr-16"
+            >
+              添加项目
+            </Button>
+          </div>
+        </div>
+        <div className="flex-1 px-16 overflow-auto">
+          <Table
+            rowKey="id"
+            data={templateList}
+            columns={COLUMNS}
+            loading={templateListLoading}
+            emptyTips={<EmptyTips text="暂无模版数据" className="py-32" />}
+          />
+        </div>
+      </div>
+      <RenderModal />
+    </>
+  );
+}
+
+export default observer(ProjectGroup);
