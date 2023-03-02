956b7504c12e2e2f9abf925ba4d52dda86f308d4
zhaobingyu <zhaobingyu@yunify.com>
2023-03-02 20:39:37 +0800

    feat: Workflow remove node delete branchID

--- clients/portal/modules/work-flow/content/editor/utils/node.ts
+++ clients/portal/modules/work-flow/content/editor/utils/node.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import { XYPosition, Node, FlowElement, removeElements, isNode, Elements } from 'react-flow-renderer';
 import { cloneDeep, set } from 'lodash';
 
@@ -227,9 +228,10 @@ export function onRemoveNode(
   newElements = updateParentAndChildNodeElementRelationship(
     newElements, elementToRemove, parentID, childrenID, isRemoveLastBranch,
   );
-  const _removedElements = removeElements([elementToRemove], newElements).map((item: any)=> {
-    const sourceElement = getNodeElementById(item?.data?.nodeDate?.parentID?.[0]);
-    const targetElement = getNodeElementById(item?.data?.nodeDate?.childrenID?.[0]);
+  const _removedElements = removeElements([elementToRemove], newElements);
+  const _filterElements = _removedElements.map((item: any)=> {
+    const sourceElement: any = _removedElements.find(({ id })=> id === item?.data?.nodeData?.parentID?.[0]);
+    const targetElement: any = _removedElements.find(({ id })=> id === item?.data?.nodeData?.childrenID?.[0]);
     const branchID = getBranchID(sourceElement, targetElement);
     const branchTargetElementID = getBranchTargetElementID(sourceElement, targetElement);
     if (!branchID || !branchTargetElementID) {
@@ -238,6 +240,22 @@ export function onRemoveNode(
     }
     return item;
   });
+
+  const temp = _filterElements.map((item: any)=>{
+    const sourceElement: any = _filterElements.find(({ id })=> id === item?.data?.nodeData?.parentID?.[0]);
+    if (item?.data?.nodeData?.branchID && _filterElements.findIndex((n)=>n.id === item?.data?.nodeData?.branchID) === -1) {
+      const { data: { nodeData } } = sourceElement;
+      item.data.nodeData = {
+        ...item.data.nodeData,
+        branchID: nodeData?.branchID,
+        branchTargetElementID: nodeData?.branchTargetElementID,
+      };
+    }
+    return {
+      ...item,
+    };
+  });
+
   return removeElements([elementToRemove], newElements);
 }
 
