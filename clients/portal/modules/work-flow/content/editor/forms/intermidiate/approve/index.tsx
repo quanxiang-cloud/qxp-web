d7d5b1adb929c9bcab84799d9bff6614f458837d
Mark <dgwhangzhou@gmail.com>
2023-04-12 12:16:57 +0800

    chore: Remove operatorPermission

--- clients/portal/modules/work-flow/content/editor/forms/intermidiate/approve/index.tsx
+++ clients/portal/modules/work-flow/content/editor/forms/intermidiate/approve/index.tsx
@@ -11,7 +11,6 @@ import { DeadLine, WhenTimeout } from '@flow/content/editor/type';
 
 import BasicConfig from '../components/basic-config';
 import FieldPermission from '../components/field-permission';
-import OperatorPermission from '../components/operator-permission';
 
 interface Props {
   defaultValue: FillInData;
@@ -89,16 +88,6 @@ export default function ApproveForm({
             <FieldPermission
               value={value.fieldPermission}
               onChange={handleChange}
-            />
-          ),
-        }, {
-          id: 'operatorPermission',
-          name: '操作权限',
-          content: (
-            <OperatorPermission
-              value={value.operatorPermission}
-              onChange={handleChange}
-              type={nodeType}
             />
           ),
         }]}
