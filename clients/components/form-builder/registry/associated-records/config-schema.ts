5b9233708ab90516de1991f2c8bf23b092de3e3a
ZhaoBingyu <13843076612@163.com>
2023-06-19 10:50:17 +0800

    feat: Update associated-records

--- clients/components/form-builder/registry/associated-records/config-schema.ts
+++ clients/components/form-builder/registry/associated-records/config-schema.ts
@@ -124,13 +124,13 @@ const schema: ISchema = {
           'x-component': 'Switch',
           'x-index': 8,
         },
-        addNewRecords: {
-          title: '新建关联记录',
-          default: false,
-          visible: false,
-          'x-component': 'Switch',
-          'x-index': 9,
-        },
+        // addNewRecords: {
+        //   title: '新建关联记录',
+        //   default: false,
+        //   visible: false,
+        //   'x-component': 'Switch',
+        //   'x-index': 9,
+        // },
         filterConfig: {
           title: '',
           'x-component': 'FilterConfig',
